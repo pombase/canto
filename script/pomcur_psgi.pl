5c9a2d3233dc3f4183c8e5f1064416e032e02531
Kim Rutherford <kmr44@cam.ac.uk>
2010-07-20 18:01:39 +0100

    Now use Plack::Middleware::ReverseProxy.

--- script/pomcur_psgi.pl
+++ script/pomcur_psgi.pl
@@ -2,5 +2,15 @@
 use strict;
 use PomCur;
 
+use Plack::Builder;
+
 PomCur->setup_engine('PSGI');
 my $app = sub { PomCur->run(@_) };
+
+builder {
+  enable_if { $_[0]->{REMOTE_ADDR} eq '127.0.0.1' }
+    "Plack::Middleware::ReverseProxy";
+  $app;
+};
+
+
