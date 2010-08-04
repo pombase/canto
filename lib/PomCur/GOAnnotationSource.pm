62fe334f34c12712021e2c42b37f7f7f1dd411e4
Kim Rutherford <kmr44@cam.ac.uk>
2010-08-04 10:54:32 +0100

    Added interfaces for accessing gene and annotation information.

---
+++ lib/PomCur/GOAnnotationSource.pm
@@ -0,0 +1,52 @@
+package PomCur::GOAnnotationSource
+
+=head1 NAME
+
+PomCur::GOAnnotationSource -
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc %( (insert perl-insert-name) %)
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use strict;
+use warnings;
+use Carp;
+
+use Moose;
+
+=head2 annotation_of_genes
+
+  Returns   : an array: [{ identifier => 'spc123', annotation => {}},
+                         {
+
+=cut
+method annotation_of_genes(\@gene_identifiers, { options ... });
+
+1;
---
+++ lib/PomCur/GeneInfoSource.pm
@@ -0,0 +1,53 @@
+package PomCur::GeneInfoSource.pm
+
+=head1 NAME
+
+PomCur::GeneInfoSource -
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc %( (insert perl-insert-name) %)
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use strict;
+use warnings;
+use Carp;
+
+use Moose;
+
+=head2
+
+ Returns   : an array: [{ identifier => 'spbc...', name => 'cdc11',
+                          chromosome => 'chr1', length => ..., ....},
+                        { identifier => ... }]
+
+=cut
+method gene_lookup(\@search_terms, { lookup_type => 'name,identifier'|'all' });
+
+1;
