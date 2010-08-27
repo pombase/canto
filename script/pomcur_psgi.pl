875636148e5ab2aa63701bd447e6a81056369e39
Kim Rutherford <kmr44@cam.ac.uk>
2010-08-27 16:53:55 +0100

    Set expires header for css and .js files

--- script/pomcur_psgi.pl
+++ script/pomcur_psgi.pl
@@ -12,13 +12,15 @@ use Plack::Builder;
 PomCur->setup_engine('PSGI');
 my $app = sub { PomCur->run(@_) };
 
+my $type_re = qr!^image/|^application/javascript$|^text/css$!i;
+
 builder {
   enable_if { $_[0]->{REMOTE_ADDR} eq '127.0.0.1' }
     "Plack::Middleware::ReverseProxy";
   enable_if { $ENV{POMCUR_DEBUG} }
     "Plack::Middleware::Debug";
   enable 'Expires',
-    content_type => qr!^image/!i,
+    content_type => $type_re,
     expires => 'access plus 12 months';
   $app;
 };
