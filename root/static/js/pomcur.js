c271b557a29ddd5391d74423ea2ee3d83e23451a
Kim Rutherford <kmr44@cam.ac.uk>
2010-09-10 14:23:36 +0100

    Formatting fixes

--- root/static/js/pomcur.js
+++ root/static/js/pomcur.js
@@ -5,34 +5,33 @@ $(document).ready(function() {
       $(this).toggleClass('undisclosed-title');
       $(this).toggleClass('disclosed-title');
       return false;
-        }
-        );
+    });
     if ($(this).is(".undisclosed-title")) {
-        $(this).next().hide();
+      $(this).next().hide();
     } else {
-        $(this).next().show();
+      $(this).next().show();
     }
   })
 });
 
 $(function() {
-    var cache = {};
-    $( "#ontology-term-entry" ).autocomplete({
-        minLength: 0,
-        source: ontology_complete_url,
-        focus: function(event, ui) {
-            $('#ontology-term-entry').val(ui.item.match);
-            return false;
-        },
-        select: function(event, ui) {
-            $('#ontology-term-entry').val(ui.item.match);
-            return false;
-        }
-    })
-    .data( "autocomplete" )._renderItem = function( ul, item ) {
-        return $( "<li></li>" )
-            .data( "item.autocomplete", item )
-            .append( "<a>" + item.match + "<br>" + item.description + "</a>" )
-            .appendTo( ul );
-    };
+  var cache = {};
+  $( "#ontology-term-entry" ).autocomplete({
+    minLength: 0,
+    source: ontology_complete_url,
+    focus: function(event, ui) {
+      $('#ontology-term-entry').val(ui.item.match);
+      return false;
+    },
+    select: function(event, ui) {
+      $('#ontology-term-entry').val(ui.item.match);
+      return false;
+    }
+  })
+  .data( "autocomplete" )._renderItem = function( ul, item ) {
+    return $( "<li></li>" )
+      .data( "item.autocomplete", item )
+      .append( "<a>" + item.match + "<br>" + item.description + "</a>" )
+      .appendTo( ul );
+  };
 });
