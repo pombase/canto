5ac2c730a2eecf041566c2ed790dfef64fcb13c7
Kim Rutherford <kmr44@cam.ac.uk>
2010-09-12 23:20:33 +0100

    Collect functions into one object

--- root/static/js/pomcur.js
+++ root/static/js/pomcur.js
@@ -14,10 +14,10 @@ $(document).ready(function() {
   })
 });
 
-$(function() {
-  var ontology_complete_url = application_root + '/ws/lookup/go/component';
+var pomcur = {
+  ontology_complete_url : application_root + '/ws/lookup/go/component',
 
-  var use_term_data = function(data) {
+  use_term_data : function(data) {
     $('#ontology-term-definition').text(data[0].definition);
 
     var children = data[0].children;
@@ -28,37 +28,40 @@ $(function() {
     });
 
     $('#ontology-term-children').html($('<ul/>').append($(children_html)));
-  }
+  },
 
-  var term_selected = function() {
+  term_selected : function() {
     var term_id = $('#ontology-term-id').val();
     if (term_id) {
       $.ajax({
-        url: ontology_complete_url + '/term',
+        url: pomcur.ontology_complete_url + '/term',
         data: { term: term_id, def: 1, children: 1 },
         dataType: 'json',
-        success: use_term_data
+        success: pomcur.use_term_data
       });
     }
-  };
+  },
 
-  var set_term = function(term) {
+  set_term : function(term) {
     $('#ontology-term-id').val(term.id);
     $('#ontology-term-entry').val(term.name);
-  };
+  }
+};
 
+
+$(function() {
   $( "#ontology-term-entry" ).autocomplete({
     minLength: 2,
-    source: ontology_complete_url + '/term',
+    source: pomcur.ontology_complete_url + '/term',
     focus: function(event, ui) {
       $('#ontology-term-entry').val(ui.item.name);
       return false;
     },
     select: function(event, ui) {
-      set_term(ui.item);
+      pomcur.set_term(ui.item);
       return false;
     },
-    close: term_selected
+    close: pomcur.term_selected
   })
   .data( "autocomplete" )._renderItem = function( ul, item ) {
     return $( "<li></li>" )
