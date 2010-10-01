6dd2b6adb6e2a6a0b97d3e936f768d7543315e52
Kim Rutherford <kmr44@cam.ac.uk>
2010-10-01 12:21:50 +0100

    Tidy up auto generated code

--- lib/PomCur/CursDB.pm
+++ lib/PomCur/CursDB.pm
@@ -17,14 +17,8 @@ __PACKAGE__->load_classes;
 # Created by DBIx::Class::Schema::Loader v0.07002 @ 2010-09-30 16:16:12
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ca0XJaFjA1pp1iyJQ6QN9Q
 
-use base 'PomCur::DB';
-
 __PACKAGE__->initialise();
 
-# You can replace this text with custom content, and it will be preserved on regeneration
-1;
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
+
 1;
--- lib/PomCur/TrackDB.pm
+++ lib/PomCur/TrackDB.pm
@@ -17,14 +17,8 @@ __PACKAGE__->load_classes;
 # Created by DBIx::Class::Schema::Loader v0.07002 @ 2010-09-30 16:18:16
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Qe5Bl/pWROrH+BZjMVLEbg
 
-use base 'PomCur::DB';
-
 __PACKAGE__->initialise();
 
-# You can replace this text with custom content, and it will be preserved on regeneration
-1;
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
+
 1;
