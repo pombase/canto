99b69e7c4077a05f7b67979799a7b5046baeb888
Kim Rutherford <kmr44@cam.ac.uk>
2011-03-04 16:55:24 +0000

    Whitespace fixes in SQL

--- etc/track.sql
+++ etc/track.sql
@@ -17,19 +17,19 @@ CREATE TABLE cv (
 );
 
 CREATE TABLE db (
-    db_id integer NOT NULL PRIMARY KEY,
-    name text NOT NULL,
-    description text,
-    urlprefix text,
-    url text
+       db_id integer NOT NULL PRIMARY KEY,
+       name text NOT NULL,
+       description text,
+       urlprefix text,
+       url text
 );
 
 CREATE TABLE dbxref (
-    dbxref_id integer NOT NULL PRIMARY KEY,
-    db_id integer NOT NULL REFERENCES db (db_id),
-    accession text NOT NULL,
-    version text NOT NULL DEFAULT '',
-    description text
+       dbxref_id integer NOT NULL PRIMARY KEY,
+       db_id integer NOT NULL REFERENCES db (db_id),
+       accession text NOT NULL,
+       version text NOT NULL DEFAULT '',
+       description text
 );
 CREATE INDEX dbxref_idx1 ON dbxref (db_id);
 CREATE INDEX dbxref_idx2 ON dbxref (accession);
@@ -48,60 +48,60 @@ CREATE INDEX cvterm_idx1 ON cvterm (cv_id);
 CREATE INDEX cvterm_idx2 ON cvterm (name);
 
 CREATE TABLE cvtermsynonym (
-    cvtermsynonym_id integer NOT NULL PRIMARY KEY,
-    cvterm_id integer NOT NULL references cvterm (cvterm_id),
-    synonym text NOT NULL,
-    type_id integer references cvterm (cvterm_id)
+       cvtermsynonym_id integer NOT NULL PRIMARY KEY,
+       cvterm_id integer NOT NULL references cvterm (cvterm_id),
+       synonym text NOT NULL,
+       type_id integer references cvterm (cvterm_id)
 );
 CREATE INDEX cvtermsynonym_idx1 ON cvtermsynonym (cvterm_id);
 
 CREATE TABLE cvterm_relationship (
-    cvterm_relationship_id integer NOT NULL PRIMARY KEY,
-    type_id integer NOT NULL references cvterm (cvterm_id),
-    subject_id integer NOT NULL references cvterm (cvterm_id),
-    object_id integer NOT NULL references cvterm (cvterm_id)
+       cvterm_relationship_id integer NOT NULL PRIMARY KEY,
+       type_id integer NOT NULL references cvterm (cvterm_id),
+       subject_id integer NOT NULL references cvterm (cvterm_id),
+       object_id integer NOT NULL references cvterm (cvterm_id)
 );
 CREATE INDEX cvterm_relationship_idx1 ON cvterm_relationship (type_id);
 CREATE INDEX cvterm_relationship_idx2 ON cvterm_relationship (subject_id);
 CREATE INDEX cvterm_relationship_idx3 ON cvterm_relationship (object_id);
 
 CREATE TABLE cvtermprop (
-    cvtermprop_id integer NOT NULL PRIMARY KEY,
-    cvterm_id integer NOT NULL references cvterm (cvterm_id),
-    type_id integer NOT NULL references cvterm (cvterm_id),
-    value text DEFAULT '' NOT NULL,
-    rank integer DEFAULT 0 NOT NULL
+       cvtermprop_id integer NOT NULL PRIMARY KEY,
+       cvterm_id integer NOT NULL references cvterm (cvterm_id),
+       type_id integer NOT NULL references cvterm (cvterm_id),
+       value text DEFAULT '' NOT NULL,
+       rank integer DEFAULT 0 NOT NULL
 );
 CREATE INDEX cvtermprop_idx1 ON cvtermprop (cvterm_id);
 CREATE INDEX cvtermprop_idx2 ON cvtermprop (type_id);
 
 CREATE TABLE cvterm_dbxref (
-    cvterm_dbxref_id integer NOT NULL PRIMARY KEY,
-    cvterm_id integer NOT NULL,
-    dbxref_id integer NOT NULL,
-    is_for_definition integer DEFAULT 0 NOT NULL
+       cvterm_dbxref_id integer NOT NULL PRIMARY KEY,
+       cvterm_id integer NOT NULL,
+       dbxref_id integer NOT NULL,
+       is_for_definition integer DEFAULT 0 NOT NULL
 );
 CREATE UNIQUE INDEX cvterm_dbxref_c1 on cvterm_dbxref (cvterm_id, dbxref_id);
 CREATE INDEX cvterm_dbxref_idx1 ON cvterm_dbxref (cvterm_id);
 CREATE INDEX cvterm_dbxref_idx2 ON cvterm_dbxref (dbxref_id);
 
 CREATE TABLE organism (
-       	organism_id integer NOT NULL PRIMARY KEY,
-        abbreviation varchar(255) null,
-        genus varchar(255) NOT NULL,
-        species varchar(255) NOT NULL,
-        common_name varchar(255) null,
-        comment text null
+       organism_id integer NOT NULL PRIMARY KEY,
+       abbreviation varchar(255) null,
+       genus varchar(255) NOT NULL,
+       species varchar(255) NOT NULL,
+       common_name varchar(255) null,
+       comment text null
 );
 
 CREATE INDEX organism_idx1 ON organism (organism_id);
 
 CREATE TABLE organismprop (
-organismprop_id integer NOT NULL PRIMARY KEY,
-organism_id integer NOT NULL REFERENCES organism (organism_id),
-type_id integer NOT NULL REFERENCES cvterm (cvterm_id),
-value text,
-rank integer DEFAULT 0 NOT NULL
+       organismprop_id integer NOT NULL PRIMARY KEY,
+       organism_id integer NOT NULL REFERENCES organism (organism_id),
+       type_id integer NOT NULL REFERENCES cvterm (cvterm_id),
+       value text,
+       rank integer DEFAULT 0 NOT NULL
 );
 
 CREATE INDEX organismprop_idx1 ON organismprop (organism_id);
@@ -144,9 +144,9 @@ CREATE TABLE lab (
 );
 
 CREATE TABLE sessions (
-       id           text PRIMARY KEY,
+       id text PRIMARY KEY,
        session_data text,
-       expires      INTEGER
+       expires INTEGER
 );
 
 CREATE TABLE gene (
