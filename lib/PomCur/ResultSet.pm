4e65260d2b86079b52f26e54c7ffbc1fb11000a6
Kim Rutherford <kmr44@cam.ac.uk>
2011-04-17 02:14:31 +0100

    Documentation fix

--- lib/PomCur/ResultSet.pm
+++ lib/PomCur/ResultSet.pm
@@ -4,16 +4,6 @@ package PomCur::ResultSet;
 
 PomCur::DB::ResultSet - A PomCur specific sub class of ResultSet
 
-=head1 SYNOPSIS
-
-DBIx::Class v0.08010 has a problem with using relations where the relation name
-doesn't match the column name, eg. having sample.organism_id as the field name
-but referring to the field as "organism" ($sample->organism()).  If an object
-(sample) is create without the field (organism/organism_id) then if the field
-is referred to (if (defined $sample->organism()) then the Organism object is
-created.   This sub-class fixes the problem by setting all fields that aren't
-passed to the create() method to undef.
-
 =head1 AUTHOR
 
 Kim Rutherford C<< <kmr44@cam.ac.uk> >>
@@ -50,4 +40,3 @@ use Carp;
 use base qw/DBIx::Class::ResultSet::Data::Pageset/;
 
 1;
-
