dbd0e270dee3c8a932b7ac452f00231e33371168
Kim Rutherford <kmr44@cam.ac.uk>
2011-05-18 19:36:23 +0100

    Add verbose flag

--- etc/load_test_app.sh
+++ etc/load_test_app.sh
@@ -5,7 +5,7 @@ phenotype_file="../sources/phenotype_ontology/fypo.obo"
 psi_mod_file="../sources/PSI-MOD.obo"
 ro_file="etc/ro.obo"
 
-./script/pomcur_load.pl --ontology $ro_file --ontology $go_ext_url \
-                        --ontology $psi_mod_file --ontology $phenotype_file
+./script/pomcur_load.pl -v --ontology $ro_file --ontology $go_ext_url \
+                           --ontology $psi_mod_file --ontology $phenotype_file
 
-./script/pomcur_load.pl --genes ../sources/genes_and_products.txt --for-taxon=4896
+./script/pomcur_load.pl -v --genes ../sources/genes_and_products.txt --for-taxon=4896
