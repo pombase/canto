afee679bbf45fe81ac84c8881ffdb53ebed991d1
Kim Rutherford <kmr44@cam.ac.uk>
2012-02-01 04:15:55 +0000

    Delete sessions with no assigned_curator

--- etc/clean_test_sessions.pl
+++ etc/clean_test_sessions.pl
@@ -70,7 +70,8 @@ while (my ($curs, $cursdb) = $iter->()) {
 
   if ($pub_uniquename eq $test_publication_uniquename ||
       $curs_key =~ /^aaaa000\d$/ ||
-      grep { $_ eq $pub_uniquename } @command_line_pubs) {
+      grep { $_ eq $pub_uniquename } @command_line_pubs ||
+      !defined $curs->assigned_curator()) {
     warn "deleting ", $curs->curs_key(), "\n";
     PomCur::Track::delete_curs($config, $track_schema, $curs_key);
   }
