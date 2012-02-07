109d42750d7330857ae29b0d00ec54a2a788aad0
Kim Rutherford <kmr44@cam.ac.uk>
2012-02-07 23:56:45 +0000

    Longer message for set_state() error

--- lib/PomCur/Curs/State.pm
+++ lib/PomCur/Curs/State.pm
@@ -201,7 +201,7 @@ sub set_state
     when (NEEDS_APPROVAL) {
       if (!$force and $current_state ne CURATION_IN_PROGRESS) {
         croak "trying to approve a session that isn't in the state ",
-          CURATION_IN_PROGRESS;
+          CURATION_IN_PROGRESS, " it's currently: ", $current_state;
       }
       $self->set_metadata($schema, NEEDS_APPROVAL_TIMESTAMP_KEY, _get_datetime());
       $self->unset_metadata($schema, APPROVAL_IN_PROGRESS_TIMESTAMP_KEY);
