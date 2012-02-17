0f5022977222d20d3329d3719218a6ef1f194a6f
Kim Rutherford <kmr44@cam.ac.uk>
2012-02-17 08:36:36 +0000

    Make compiled Mason obj directory unique(r)

--- lib/PomCur/View/Mason.pm
+++ lib/PomCur/View/Mason.pm
@@ -8,6 +8,7 @@ use File::Spec;
 use PomCur::Config;
 
 my $app = PomCur::Config::get_application_name();
+my $inst_name = PomCur::Config::get_instance_name();
 
 __PACKAGE__->config(use_match => 0);
 __PACKAGE__->config(escape_flags => {
@@ -17,7 +18,7 @@ __PACKAGE__->config(default_escape_flags => 'html');
 __PACKAGE__->config->{data_dir} =
   File::Spec->catdir(
     File::Spec->tmpdir,
-    sprintf('%s_%d_mason_data_dir', $app, $<)
+    sprintf('%s_%d_%s_mason_data_dir', $app, $<, $inst_name)
   );
 
 =head1 NAME
