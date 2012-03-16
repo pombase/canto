pombase/canto

commit 38b5c7e31d08370a76d73e0aae55daaea4ee503f

Fix ontology loading for ID <> name

lib/PomCur/Track/OntologyLoad.pm
@@ -213,7 +213,7 @@ sub load
                                               $term->is_relationship_type());
 
         if ($term->is_relationship_type()) {
-          $relationship_cvterms{$term->name()} = $cvterm;
+          $relationship_cvterms{$term->acc()} = $cvterm;
         }
 
         my $cvterm_id = $cvterm->cvterm_id();

commit a994282377e629ce0b126e4682c5a308614a9c2f

Fix for missing isa_rel caused by [38b5c7e31d08]

lib/PomCur/Track/OntologyLoad.pm
@@ -217,7 +217,8 @@ sub load
                                               $term->is_relationship_type());
 
         if ($term->is_relationship_type()) {
-          $relationship_cvterms{$term->acc()} = $cvterm;
+          (my $term_acc = $term->acc()) =~ s/OBO_REL://;
+          $relationship_cvterms{$term_acc} = $cvterm;
         }
 
         my $cvterm_id = $cvterm->cvterm_id();