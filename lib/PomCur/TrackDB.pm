7189b491bc182c0e2ead1dd205c2b9780f76f714
Kim Rutherford <kmr44@cam.ac.uk>
2012-03-25 11:41:59 +0100

    Remove redundant code

--- lib/PomCur/TrackDB.pm
+++ lib/PomCur/TrackDB.pm
@@ -20,9 +20,5 @@ __PACKAGE__->load_classes;
 __PACKAGE__->initialise();
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
-1;
 
-
-# You can replace this text with custom content, and it will be preserved on regeneration
-__PACKAGE__->meta->make_immutable;
 1;
