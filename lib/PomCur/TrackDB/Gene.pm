12d7ce33d2ec8790e1ac4c057fa52e2bf9f99739
Kim Rutherford <kmr44@cam.ac.uk>
2012-04-12 05:49:56 +0100

    Remove redundant code

--- lib/PomCur/TrackDB/Gene.pm
+++ lib/PomCur/TrackDB/Gene.pm
@@ -135,9 +135,5 @@ use Moose;
 with 'PomCur::Role::GeneNames';
 
 # You can replace this text with custom content, and it will be preserved on regeneration
-1;
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
 1;
