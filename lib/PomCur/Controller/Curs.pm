50b40ea1204e93e48b6a16da29b235ed6b761ea0
Kim Rutherford <kmr44@cam.ac.uk>
2012-05-18 10:50:36 +0100

    Fix stash typo in [ffc43521b0eaab8]

--- lib/PomCur/Controller/Curs.pm
+++ lib/PomCur/Controller/Curs.pm
@@ -626,7 +626,7 @@ sub gene_upload : Chained('top') Args(0) Form
         $c->detach();
         return 0;
       } else {
-        $c->flash()->{hightlight_terms} = [@search_terms];
+        $c->flash()->{highlight_terms} = [@search_terms];
         _redirect_and_detach($c, 'confirm_genes');
       }
     }
