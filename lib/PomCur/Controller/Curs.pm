bfc77c2dcd974e5eb277c8ffa8529201c7d6b783
Kim Rutherford <kmr44@cam.ac.uk>
2013-04-17 07:43:26 +1200

    Change subject of session (re-)assign email

--- lib/PomCur/Controller/Curs.pm
+++ lib/PomCur/Controller/Curs.pm
@@ -2735,14 +2735,17 @@ sub _assign_session :Private
 
     $self->state()->store_statuses($schema);
 
-    my $subject = "A PomBase publication has been assigned to you for community curation";
+    my $st = $c->stash();
+    my $pub_uniquename = $st->{pub}->uniquename();
 
     if ($reassign) {
+      my $subject = "Invitation to curate $pub_uniquename in PomBase";
       _send_mail($self, $c, subject => $subject, to => 'admin');
       _send_mail($self, $c, subject => $subject, to => $submitter_email);
 
       _redirect_and_detach($c, 'session_reassigned');
     } else {
+      my $subject = "Link forcurate $pub_uniquename in PomBase";
       _send_mail($self, $c, subject => $subject, to => $submitter_email);
       _redirect_and_detach($c);
     }
