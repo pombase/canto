pombase/canto

commit e065d6495aacc04afac531b5093b91ccccbf5ea9

Add email templates for PomBase

lib/PomCur/Curs/Utils.pm
@@ -39,6 +39,7 @@ use strict;
 use warnings;
 use Carp;
 use Moose;
+use Clone qw(clone);
 
 use PomCur::Curs::GeneProxy;
 
@@ -623,7 +624,7 @@ sub get_existing_interaction_annotations
 =head2 get_existing_annotations
 
  Usage   :
-   my @annotations =
+   my ($all_annotations_count, $annotations) =
      PomCur::Curs::Utils::get_existing_annotations($config, $options);
  Function: Return a table of the existing interaction annotations from the
            database
@@ -634,8 +635,6 @@ sub get_existing_interaction_annotations
                the search; only annotations for the gene are returned (optional)
            $options->{annotation_type_name} - the annotation type eg.
                'biological_process', 'physical_interaction'
-           $options->{annotation_type_category} - the annotation category, eg.
-               'ontology' or 'interaction'
  Returns : An array of hashes containing the annotation in the same form as
            get_annotation_table() above, except that annotation_id will be a
            database identifier for the annotation.
@@ -646,11 +645,43 @@ sub get_existing_annotations
   my $config = shift;
   my $options = shift;
 
-  if ($options->{annotation_type_category} eq 'ontology') {
+  my $annotation_type_category =
+    $config->{annotation_types}->{$options->{annotation_type_name}};
+
+  if ($annotation_type_category eq 'ontology') {
     return get_existing_ontology_annotations($config, $options);
   } else {
     return get_existing_interaction_annotations($config, $options);
   }
+}
+
+=head2 get_existing_annotation_count
+
+ Usage   : my $count = PomCur::Curs::Utils::get_existing_annotation_count($config, $options);
+ Function: Return the total number of existing annotations for a publication
+ Args    : $config - the PomCur::Config object
+           $options -
+             $options->{pub_uniquename} - the publication ID (eg. PubMed ID)
+                 to count annotations of
+ Return  : the count
+
+=cut
+sub get_existing_annotation_count
+{
+  my $config = shift;
+  my $arg_options = shift;
+
+  my $count = 0;
+
+  for my $annotation_type (@{$config->{annotation_type_list}}) {
+    my $options = clone $arg_options;
+    $options->{annotation_type_name} = $annotation_type->{name};
+    my ($all_annotations_count, $annotations) =
+      PomCur::Curs::Utils::get_existing_annotations($config, $options);
+    $count += $all_annotations_count;
+  }
+
+  return $count;
 }
 
 =head2 store_all_statuses

lib/PomCur/EmailUtil.pm
@@ -44,6 +44,8 @@ use HTML::Mason;
 
 with 'PomCur::Role::Configurable';
 
+use PomCur::Curs::Utils;
+
 sub _process_template
 {
   my $self = shift;
@@ -104,6 +106,14 @@ sub make_email_contents
   }
 
   $args{config} = $self->config();
+
+  my %options = ( max_results => 1,
+                  pub_uniquename => $args{publication_uniquename} );
+
+  my ($all_existing_annotations_count, $existing_annotations) =
+    PomCur::Curs::Utils::get_existing_annotation_count($self->config(), \%options);
+
+  $args{existing_annotation_count} = $all_existing_annotations_count;
 
   my $subject = $self->_process_template($interp, $subject_component_path, %args);
   chomp $subject;

root/email_templates/generic/session_reassigned_body.mhtml
@@ -5,6 +5,7 @@ $publication_title
 $session_link
 $curator_name
 $help_index
+$existing_annotation_count
 </%args>
 Dear <% $curator_name %>,
 
@@ -22,6 +23,15 @@ When you follow the above link and sign in, you will be guided step-by-step
 though the annotation process. You can annotate these data types in the tool:
 
 <& ../annotation_type_list.mhtml, config => $config &>
+
+You will also be able to modify the annotations your colleague made.
+
+% if ($existing_annotation_count > 0) {
+For this paper, there are also several previously curated annotations
+in the database. These existing annotations will be shown in the
+curation tool, but cannot be altered. If you notice any problems with existing
+annotations, please let the curators know.
+% }
 
 Help is available on each page, and you can also read
 the Canto manual (<% $help_index %>) or contact

root/email_templates/pombase/session_accepted_body.mhtml
@@ -1,0 +1,19 @@
+<%args>
+$config
+$publication_uniquename
+$publication_title
+$session_link
+$curator_name
+$help_index
+</%args>
+Thank you for agreeing to curate:
+<% $publication_uniquename %> - "<% $publication_title %>"
+
+You can return to your curation session at any time with this link:
+
+<% $session_link %>
+
+PomBase
+
+
+

root/email_templates/pombase/session_accepted_subject.mhtml
@@ -1,0 +1,1 @@
+A research publication has been assigned to you for community curation

root/email_templates/pombase/session_assigned_body.mhtml
@@ -9,11 +9,11 @@ $existing_annotation_count
 </%args>
 Dear <% $curator_name %>,
 
-The curation link is:
-
 Congratulations on the recent publication of your paper,
-<% $publication_uniquename %> - "<% $publication_title %>".
-We invite you to contribute annotations based on this paper.
+<% $publication_uniquename %> - "<% $publication_title %>".  We invite you to
+contribute annotations based on this paper to PomBase.  Any annotations you
+make will be prioritised for inclusion in PomBase, and will therefore also be
+readily visible in other databases such as GO, UniProtKB, BioGRID, etc.
 
 Your curation link is:
 
@@ -21,7 +21,9 @@ Your curation link is:
 
 When you follow the above link and sign in, you will be guided step-by-step
 though the annotation process. You can annotate these data types in the tool:
+
 <& ../annotation_type_list.mhtml, config => $config &>
+
 % if ($existing_annotation_count > 0) {
 For this paper, there are several previously curated annotations in the
 database. These existing annotations will be shown in the curation tool, but
@@ -31,7 +33,7 @@ please let the curators know.
 
 Help is available on each page, and you can also read
 the Canto manual (<% $help_index %>) or contact
-the help desk (<% $config->{contact_email}->{address} %>)
+the PomBase staff (mailto:<% $config->{contact_email}->{address} %>)
 for assistance at any time. You will also have an opportunity to provide any
 data that the tool does not capture so that curators can include it with your
 curation.
@@ -41,7 +43,11 @@ same URL as often as you want. You can also invite a co-author to help curate
 the paper by changing the "session assignment" before submitting your
 annotations.
 
-We greatly appreciate your contribution.
-
+We greatly appreciate your contribution -- all community annotations help keep
+PomBase literature curation comprehensive, accurate, and up to date.
 
 <% $session_link %>
+
+Sincerely yours,
+
+The PomBase Team

root/email_templates/pombase/session_assigned_subject.mhtml
@@ -1,0 +1,1 @@
+Invitation to curate your paper in PomBase

root/email_templates/pombase/session_reassigned_body.mhtml
@@ -1,0 +1,86 @@
+<%args>
+$config
+$publication_uniquename
+$publication_title
+$session_link
+$curator_name
+$help_index
+$existing_annotation_count
+</%args>
+Dear <% $curator_name %>,
+
+A colleague has invited you to contribute annotations to PomBase based on this
+paper:
+
+<% $publication_uniquename %> - "<% $publication_title %>".
+
+The curation link is:
+
+<% $session_link %>
+
+When you follow the above link and sign in, you will see any annotations that
+your co-author has already made, and you will be guided step-by-step though
+the process to add more annotations. You can annotate these data types in the
+tool:
+
+<& ../annotation_type_list.mhtml, config => $config &>
+
+You will also be able to modify the annotations your colleague made.
+
+% if ($existing_annotation_count > 0) {
+For this paper, there are also several previously curated annotations in the
+database.  These existing annotations will be shown in the curation tool, but
+cannot be altered. If you notice any problems with existing annotations,
+please let the curators know.
+% }
+
+Help is available on each page, and you can also read the Canto manual here
+[link] or contact the PomBase staff for assistance at any time. You will also
+have an opportunity to provide any data that the tool does not capture so that
+curators can include it with your curation.
+
+The curation link will work indefinitely, so you can leave and return to the
+same URL as often as you want. When you have added all the annotations you
+can, you may either submit the annotations to the PomBase curators, or send
+the paper to another co-author for further curation.
+
+We greatly appreciate your contribution -- all community annotations help keep
+PomBase literature curation comprehensive, accurate, and up to date.
+
+<% $session_link %>
+
+Sincerely yours,
+
+The PomBase Team
+
+
+The curation link is:
+
+Congratulations on the recent publication of your paper,
+<% $publication_uniquename %> - "<% $publication_title %>".
+We invite you to contribute annotations based on this paper.
+
+Your curation link is:
+
+<% $session_link %>
+
+When you follow the above link and sign in, you will be guided step-by-step
+though the annotation process. You can annotate these data types in the tool:
+
+<& ../annotation_type_list.mhtml, config => $config &>
+
+Help is available on each page, and you can also read
+the Canto manual (<% $help_index %>) or contact
+the help desk (<% $config->{contact_email}->{address} %>)
+for assistance at any time. You will also have an opportunity to provide any
+data that the tool does not capture so that curators can include it with your
+curation.
+
+The curation link will work indefinitely, so you can leave and return to the
+same URL as often as you want. You can also invite a co-author to help curate
+the paper by changing the "session assignment" before submitting your
+annotations.
+
+We greatly appreciate your contribution.
+
+<% $session_link %>

commit 6c3210507785006c58792fa9f0b8f4d315b4de92

Fix missing annotation caused by [e065d649]

lib/PomCur/Curs/Utils.pm
@@ -646,7 +646,7 @@ sub get_existing_annotations
   my $options = shift;
 
   my $annotation_type_category =
-    $config->{annotation_types}->{$options->{annotation_type_name}};
+    $config->{annotation_types}->{$options->{annotation_type_name}}->{category};
 
   if ($annotation_type_category eq 'ontology') {
     return get_existing_ontology_annotations($config, $options);