pombase/canto

commit a5ee2e1204a77290cd7f07722911502196f8d3b9

Allow varying the from address in emails

lib/PomCur/MailSender.pm
@@ -53,6 +53,8 @@ with 'PomCur::Role::Configurable';
  Args    : subject - email subject
            body - email body or undef
            to - email recipient
+           from - (optional) the sender - defaults to email->from_address in the
+                                          config file
  Return  : nothing, failures are logged
 
 =cut
@@ -61,6 +63,7 @@ sub send
   my $self = shift;
   my %args = @_;
   my $to = $args{to};
+  my $from = $args{from};
 
   if ($self->config()->{test_mode} || $::test_mode) {
     return;
@@ -68,17 +71,6 @@ sub send
 
   my $subject = $args{subject} // 'no_subject';
   my $body = $args{body} // '';
-
-  my $config = $self->config();
-
-  my $email_config = $config->{email};
-
-  if (!defined $email_config) {
-    warn "email not configured - email to admin not sent\n";
-    return;
-  }
-
-  my $from = $email_config->{from_address};
 
   if (!defined $from) {
     warn "'from' email address not configured - email with subject " .
@@ -98,8 +90,8 @@ sub send
   try {
     sendmail($email,
              {
-               from=>$from,
-               transport=>Email::Sender::Transport::Sendmail->new
+               from => $from,
+               transport => Email::Sender::Transport::Sendmail->new()
              });
   } catch {
     warn qq|Cannot send mail to "$to" with subject "$subject": $_|;

pomcur.yaml
@@ -48,6 +48,9 @@ email:
       subject: "email_templates/generic/session_accepted_subject.mhtml"
       body: "email_templates/generic/session_accepted_body.mhtml"
     session_assigned:
+      # Set the "From" to be the current, logged in admin user to make
+      # messages more personal - see SourceForge #604
+      from_address: CURRENT_USER
       subject: "email_templates/generic/session_assigned_subject.mhtml"
       body: "email_templates/generic/session_assigned_body.mhtml"
     session_reassigned:

lib/PomCur/Controller/Curs.pm
@@ -2899,6 +2899,7 @@ sub _send_mail
   my $body = $args{body};
   my $subject = $args{subject};
   my $dest_email = $args{to};
+  my $from = $args{from};
 
   my $config = $c->config();
   my $mail_sender = PomCur::MailSender->new(config => $config);
@@ -2908,6 +2909,7 @@ sub _send_mail
                                 body => $body);
   } else {
     $mail_sender->send(to => $dest_email,
+                       from => $from,
                        subject => $subject,
                        body => $body);
   }
@@ -2947,9 +2949,10 @@ sub _send_email_from_template
 
   my $recipient_email = $args{recipient_email} // $submitter_email;
 
-  my ($subject, $body) = $email_util->make_email_contents($type, %args);
+  my ($subject, $body, $from) = $email_util->make_email($type, %args);
 
-  $self->_send_mail($c, subject => $subject, body => $body, to => $recipient_email);
+  $self->_send_mail($c, subject => $subject, body => $body, to => $recipient_email,
+                    from => $from);
 }
 
 sub begin_approval : Chained('top') Args(0)

lib/PomCur/Controller/Tools.pm
@@ -819,7 +819,7 @@ sub send_session : Local Args(1)
     logged_in_user => $c->user(),
   );
 
-  my ($subject, $body) = $email_util->make_email_contents('session_assigned', %args);
+  my ($subject, $body) = $email_util->make_email('session_assigned', %args);
 
   my $mail_sender = PomCur::MailSender->new(config => $config);
 
@@ -1013,7 +1013,7 @@ sub _daily_summary_text : Private
               summary_date => $summary_date,
               app_prefix => $app_prefix);
 
-  my ($subject, $body) = $email_util->make_email_contents('daily_summary', %args);
+  my ($subject, $body) = $email_util->make_email('daily_summary', %args);
 
   return $subject . "\n\n" . $body;
 }

lib/PomCur/EmailUtil.pm
@@ -62,9 +62,9 @@ sub _process_template
   return $buffer;
 }
 
-=head2 make_email_contents
+=head2 make_email
 
- Usage   : my ($subject, $body) = $email_util->make_email_contents($type, %args);
+ Usage   : my ($subject, $body, $from) = $email_util->make_email($type, %args);
  Function: Return the subject and body text to send to users in various
            situations
  Args    : $type - the type of email to compose, one of:
@@ -81,7 +81,8 @@ sub _process_template
                     - track_schema - a TrackDB object
                     - session_link - full URL of the session
                     - curator_name - the user currently curating that session
-                    - curator_email - the email of the user
+                    - curator_email - the email of the user - the recipient of
+                                      this email
                     - reassigner_name - for the "reassigner" template, the name
                                         of the person doing the reassigning
                     - publication_uniquename - the PMID ID of the publication
@@ -94,7 +95,7 @@ sub _process_template
 
 =cut
 
-sub make_email_contents
+sub make_email
 {
   my $self = shift;
   my $email_type = shift;
@@ -137,7 +138,33 @@ sub make_email_contents
 
   $body =~ s/\n\n+/\n\n/g;
 
-  return ($subject, $body);
+  my $from_email_address = undef;
+
+  my $type_from = $type_config->{from_address};
+
+  if (defined $type_from) {
+    if ($type_from eq 'CURRENT_USER') {
+      $from_email_address = $args{logged_in_user}->email_address();
+    } else {
+      $from_email_address = $type_from;
+    }
+  }
+
+  if (!defined $from_email_address) {
+    my $config = $self->config();
+
+    my $email_config = $config->{email};
+
+    if (!defined $email_config) {
+      warn "email addresses not configured - email not sent\n";
+      return;
+    }
+
+    # use the default from address
+    $from_email_address = $email_config->{from_address};
+  }
+
+  return ($subject, $body, $from_email_address);
 }
 
 1;

script/pomcur_daily.pl
@@ -77,7 +77,7 @@ my %args = (track_schema => $track_schema,
             summary_date => $summary_date,
             app_prefix => $canto_base_url);
 
-my ($subject, $body) = $email_util->make_email_contents('daily_summary', %args);
+my ($subject, $body) = $email_util->make_email('daily_summary', %args);
 
 if ($send_mail) {
   $mail_sender->send_to_admin(subject => $subject,

t/70_email_util.t
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Test::More tests => 11;
+use Test::More tests => 14;
 
 use Test::MockObject;
 
@@ -25,9 +25,11 @@ my $curator_name = "Val Wood";
 my $curator_email = 'val@example.com';
 
 my $person_name = 'Logged In Person';
+my $person_email = 'mock_email@example.com';
 
 my $mock_person = Test::MockObject->new();
 $mock_person->mock('name', sub { return $person_name; });
+$mock_person->mock('email_address', sub { return $person_email; });
 
 my %args = (
   session_link => $root_url,
@@ -39,10 +41,17 @@ my %args = (
   logged_in_user => $mock_person,
 );
 
-$config->{email}->{templates}->{session_assigned}->{body} = "email_templates/pombase/session_assigned_body.mhtml";
+my $default_test_from = 'test_from@example.org';
 
-my ($subject, $body) =
-  PomCur::EmailUtil::make_email_contents($mock, 'session_assigned', %args);
+$config->{email}->{from_address} = $default_test_from;
+$config->{email}->{templates}->{session_assigned}->{body} =
+  "email_templates/pombase/session_assigned_body.mhtml";
+
+my ($subject, $body, $from) =
+  PomCur::EmailUtil::make_email($mock, 'session_assigned', %args);
+
+ok ($from ne $default_test_from);
+is ($from, $person_email);
 
 like ($subject, qr/publication has been assigned to you/);
 
@@ -58,10 +67,12 @@ $args{recipient_email} = 'test@example.com';
 $args{reassigner_name} = "Test Name";
 $args{reassigner_email} = 'test@example.com';
 
-($subject, $body) =
-  PomCur::EmailUtil::make_email_contents($mock, 'reassigner', %args);
+($subject, $body, $from) =
+  PomCur::EmailUtil::make_email($mock, 'reassigner', %args);
 
 like ($body, qr/Thank you for reassigning/);
 like ($body, qr/Below is a copy/);
 like ($body, qr/Dear Val Wood/);
 like ($body, qr/Test Name <test\@example.com> has invited/);
+
+is ($from, $default_test_from);

commit 8d264dd60afdb5539fe5e4a60f72a0c9717c7ad3

Fix daily summary broken by [a5ee2e120]

lib/PomCur/MailSender.pm
@@ -140,6 +140,7 @@ sub send_to_admin
   }
 
   $self->send(to => $admin_address,
+              from => $email_config->{noreply_address},
               subject => $subject,
               body => $body);
 }

pomcur.yaml
@@ -43,6 +43,7 @@ view_options:
 email:
   from_address: CONFIGURE_ME_IN_POMCUR_DEPLOY.YAML
   admin_address: CONFIGURE_ME_IN_POMCUR_DEPLOY.YAML
+  noreply_address: CONFIGURE_ME_IN_POMCUR_DEPLOY.YAML
   templates:
     session_accepted:
       subject: "email_templates/generic/session_accepted_subject.mhtml"