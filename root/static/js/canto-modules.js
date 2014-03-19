cc42b18053e8d7e0b96a4e2e65753d2990f415fe
Kim Rutherford <kmr44@cam.ac.uk>
2014-03-19 22:18:12 +1300

    Fix disabled OK button in allele dialog

--- root/static/js/canto-modules.js
+++ root/static/js/canto-modules.js
@@ -76,11 +76,11 @@ var alleleEditDialogCtrl =
     };
 
     $scope.isValidName = function() {
-      return !$scope.current_type_config || !$scope.current_type_config.allele_name_required || $scope.alleleData.name;
+      return !$scope.current_type_config || $scope.current_type_config.allele_name_required == 0 || $scope.alleleData.name;
     };
 
     $scope.isValidDescription = function() {
-      return !$scope.current_type_config || !$scope.current_type_config.description_required || $scope.alleleData.description;
+      return !$scope.current_type_config || $scope.current_type_config.description_required == 0 || $scope.alleleData.description;
     };
 
     $scope.isValid = function() {
