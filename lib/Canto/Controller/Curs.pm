pombase/canto

commit a22248a3f174bcfd7d89db620896cc19a8cbe28c

Initial support for duplicating genotypes
Refs #807

lib/Canto/Controller/Curs.pm
@@ -1698,13 +1698,15 @@ sub feature_add : Chained('feature') PathPart('add')
   $st->{template} = "curs/${feature_type}_edit.mhtml";
 }
 
-sub feature_edit : Chained('feature') PathPart('edit')
+sub _feature_edit_helper
 {
-  my ($self, $c, $genotype_id) = @_;
+  my ($self, $c, $edit_or_duplicate, $genotype_id) = @_;
 
   my $st = $c->stash();
   $st->{show_title} = 1;
   my $feature_type = $st->{feature_type};
+
+  $st->{edit_or_duplicate} = $edit_or_duplicate;
 
   my $schema = $st->{schema};
 
@@ -1769,16 +1771,36 @@ sub feature_edit : Chained('feature') PathPart('edit')
       $st->{feature} = $genotype;
       $st->{features} = [$genotype];
 
-      $st->{annotation_count} = $genotype->annotations()->count();
+      if ($edit_or_duplicate eq 'edit') {
+        $st->{annotation_count} = $genotype->annotations()->count();
+      }
 
       my $display_name = $st->{feature}->display_name();
 
-      $st->{title} = "Editing genotype: $display_name";
+      if ($edit_or_duplicate eq 'edit') {
+        $st->{title} = "Editing genotype: $display_name";
+      } else {
+        $st->{title} = "Adding a genotype";
+      }
       $st->{template} = "curs/${feature_type}_edit.mhtml";
     }
   } else {
     die "can't edit feature type: $feature_type\n";
   }
+}
+
+sub feature_duplicate : Chained('feature') PathPart('duplicate')
+{
+  my ($self, $c, $genotype_id) = @_;
+
+  $self->_feature_edit_helper($c, 'duplicate', $genotype_id);
+}
+
+sub feature_edit : Chained('feature') PathPart('edit')
+{
+  my ($self, $c, $genotype_id) = @_;
+
+  $self->_feature_edit_helper($c, 'edit', $genotype_id);
 }
 
 sub _decode_json_content

root/curs/genotype_edit.mhtml
@@ -4,13 +4,14 @@ $title
 $pub
 $multi_organism_mode
 $curs_root_uri
-$annotation_count
+$annotation_count => 0
 $genotype_id => ''
+$edit_or_duplicate
 </%args>
 
 <div class="curs-genotype-edit">
 <div ng-controller="MultiAlleleCtrl" id="curs-multi-allele-select"
-     ng-init="init('<% $genotype_id %>')";
+     ng-init="init('<% $edit_or_duplicate %>', '<% $genotype_id %>')";
      class="curs-box ng-cloak">
     <div class="curs-box-title">
 Add alleles from the genes below until the genotype is fully specified.

root/curs/genotype_page.mhtml
@@ -95,6 +95,9 @@ Description
     <div>
       <a ng-click="checkEditIsSensible($event)" href="<% $start_path . '/feature/genotype/edit/' . $genotype->genotype_id() %>">Edit ...</a>
     </div>
+    <div>
+      <a href="<% $start_path . '/feature/genotype/duplicate/' . $genotype->genotype_id() %>">Duplicate ...</a>
+    </div>
 % }
   </div>
 </div>

root/static/js/canto-modules.js
@@ -1664,16 +1664,21 @@ var multiAlleleCtrl =
     curs_config_promise: CantoConfig.get('curs_config')
   };
 
-  $scope.init = function(genotype_id) {
-    if (genotype_id) {
-      $scope.data.genotype_id = genotype_id;
+  $scope.init_from = function(genotype_id) {
+    Curs.details('genotype', ['by_id', genotype_id])
+      .success(function(genotype_details) {
+        $scope.alleles = genotype_details.alleles;
+        $scope.data.genotype_name = genotype_details.name;
+        $scope.data.genotype_background = genotype_details.background;
+      });
+  };
 
-      Curs.details('genotype', ['by_id', genotype_id])
-        .success(function(genotype_details) {
-          $scope.alleles = genotype_details.alleles;
-          $scope.data.genotype_name = genotype_details.name;
-          $scope.data.genotype_background = genotype_details.background;
-        });
+  $scope.init = function(edit_or_duplicate, genotype_id) {
+    if (genotype_id) {
+      if (edit_or_duplicate === 'edit') {
+        $scope.data.genotype_id = genotype_id;
+      }
+      $scope.init_from(genotype_id);
     }
   };
 

root/static/ng_templates/genotype_list_row.html
@@ -16,7 +16,10 @@ Use this genotype ...
       </a>
     </div>
     <div ng-if="genotype.genotype_id">
-      <a href="{{curs_root_uri + '/feature/genotype/annotate/' + genotype.id_or_identifier + '/start/phenotype'}}">Add phenotype ...</a>
+      <a href="{{curs_root_uri + '/feature/genotype/annotate/' + genotype.genotype_id + '/start/phenotype'}}">Add phenotype ...</a>
+    </div>
+    <div ng-if="genotype.genotype_id">
+      <a href="{{curs_root_uri + '/feature/genotype/duplicate/' + genotype.genotype_id}}">Duplicate ...</a>
     </div>
     <div ng-if="genotype.genotype_id">
       <a href="#" ng-click="deleteGenotype()">Delete ...</a>

commit 61e1a05edb5c8d2df45c42f45ada6825aeb8154f

Fix "Add a genotype" broken by duplicate genotype
Broken by [a22248a].

Refs #807

lib/Canto/Controller/Curs.pm
@@ -1694,6 +1694,8 @@ sub feature_add : Chained('feature') PathPart('add')
 
   $st->{annotation_count} = 0;
 
+  $st->{edit_or_duplicate} = 'edit';
+
   $st->{title} = "Add a $feature_type";
   $st->{template} = "curs/${feature_type}_edit.mhtml";
 }