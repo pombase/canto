357bd5e367620e143cf3a57bf9af943e72536347
Kim Rutherford <kmr44@cam.ac.uk>
2015-10-21 17:05:01 +1300

    Add ExtensionSubsetProcess and tests
    Refs #996

---
+++ lib/Canto/Config/ExtensionSubsetProcess.pm
@@ -0,0 +1,181 @@
+package Canto::Config::ExtensionSubsetProcess;
+
+=head1 NAME
+
+Canto::Config::ExtensionSubsetProcess - Read the domains and ranges from the
+  extension configuration and use owltools to find the child terms.  Store
+  canto_subset cvtermprops to record this.
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Canto::Config::ExtensionSubsetProcess
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use Moose;
+
+has config => (is => 'ro', isa => 'Canto::Config',
+               required => 1);
+has track_schema => (is => 'ro', isa => 'Canto::TrackDB',
+                     required => 1);
+
+use File::Temp qw/ tempfile /;
+use List::MoreUtils qw(uniq);
+
+sub get_owltools_results
+{
+  my $self = shift;
+  my $obo_file_name = shift;
+
+  my ($temp_fh, $temp_filename) = tempfile();
+
+  system ("owltools $obo_file_name --save-closure-for-chado $temp_filename") == 0
+    or die "can't open pipe from owltools: $?";
+
+  open my $owltools_out, '<', $temp_filename
+    or die "can't open owltools output from $temp_filename: $!\n";
+
+  return $owltools_out;
+}
+
+=head2 process
+
+ Usage   : $extension_subset_process->process();
+ Function: Read the domains and ranges from extension_configuration config,
+           then use owtools to find the child terms in the given OBO file.
+           Each cvterm gets a canto_subset cvtermprop for each config file
+           term it's a child of.  For more details see:
+           https://github.com/pombase/canto/wiki/AnnotationExtensionConfig
+ Args    : OBO filenames
+ Return  : None - dies on failure
+
+=cut
+
+sub process
+{
+  my $self = shift;
+  my @obo_file_names = @_;
+
+  my $config = $self->config();
+  my $schema = $self->track_schema();
+
+  my $ext_conf = $config->{extension_configuration};
+
+  if (!$ext_conf) {
+    die "no extension configuration file set\n";
+  }
+
+  my @conf = @{$ext_conf};
+
+  my %subsets = ();
+
+  for my $obo_file_name (@obo_file_names) {
+    my $pipe_from_owltools = $self->get_owltools_results($obo_file_name);
+
+    while (defined (my $line = <$pipe_from_owltools>)) {
+      chomp $line;
+      my ($subject, $rel_type, $depth, $object) =
+        split (/\t/, $line);
+
+      die $line unless $rel_type;
+
+      $rel_type =~ s/^OBO_REL://;
+
+      for my $conf (@conf) {
+        if ($conf->{domain} eq $object && $conf->{subset_rel} eq $rel_type) {
+          $subsets{$subject}{$object} = 1;
+        }
+      }
+    }
+  }
+
+  # the configuration applies to the domain term ID, not just its descendants
+  for my $conf (@conf) {
+    $subsets{$conf->{domain}}{$conf->{domain}} = 1;
+  }
+
+  my @domains = uniq map { $_->{domain}; } @conf;
+
+  my %db_names = ();
+
+  map {
+    if (/(\w+):/) {
+      $db_names{$1} = 1;
+    }
+  } keys %subsets;
+
+  my @db_names = keys %db_names;
+
+  my $cvterm_rs =
+    $schema->resultset('Cvterm')->search({
+      'db.name' => { -in => \@db_names },
+    }, {
+      join => { dbxref => 'db' },
+      prefetch => { dbxref => 'db' }
+    });
+
+  my $canto_subset_term =
+    $schema->resultset('Cvterm')->find({ name => 'canto_subset',
+                                         'cv.name' => 'cvterm_property_type' },
+                                       {
+                                         join => 'cv' });
+
+  while (defined (my $cvterm = $cvterm_rs->next())) {
+    my $db_accession = $cvterm->db_accession();
+
+    my $prop_rs =
+      $cvterm->cvtermprop_cvterms()
+      ->search({
+        type_id => $canto_subset_term->cvterm_id(),
+      });
+
+    my $guard = $schema->txn_scope_guard();
+
+    $prop_rs->delete();
+
+    my $subset_ids = $subsets{$db_accession};
+
+    if ($subset_ids) {
+      my @subset_ids = keys %{$subset_ids};
+
+      for (my $rank = 0; $rank < @subset_ids; $rank++) {
+        my $subset_id = $subset_ids[$rank];
+        $schema->resultset('Cvtermprop')->create({
+          cvterm_id => $cvterm->cvterm_id(),
+          type_id => $canto_subset_term->cvterm_id(),
+          value => $subset_id,
+          rank => $rank,
+        });
+      }
+    }
+
+    $guard->commit();
+  }
+}
+
+1;
---
+++ t/75_extension_process.t
@@ -0,0 +1,86 @@
+use strict;
+use warnings;
+use Test::More tests => 2;
+use Test::Deep;
+use Test::MockObject::Extends;
+
+use Canto::TestUtil;
+use Canto::Config::ExtensionSubsetProcess;
+
+my $test_util = Canto::TestUtil->new();
+
+$test_util->init_test();
+
+my $config = $test_util->config();
+
+my $track_schema = Canto::TrackDB->new(config => $config);
+
+my $index_path = $config->data_dir_path('ontology_index_dir');
+my $ontology_index = Canto::Track::OntologyIndex->new(index_path => $index_path);
+
+$test_util->load_test_ontologies($ontology_index, 1, 1);
+
+my $test_go_obo_file =
+  $test_util->root_dir() . '/' . $config->{test_config}->{test_go_obo_file};
+
+my $mock_request = Test::MockObject->new();
+$mock_request->mock('param', sub { return 'track' });
+
+my $processor = Canto::Config::ExtensionSubsetProcess->new(config => $config,
+                                                           track_schema => $track_schema);
+
+$processor = Test::MockObject::Extends->new($processor);
+$processor->mock('get_owltools_results',
+                 sub {
+                   open my $fh, '<', $test_util->root_dir() . '/t/data/owltools_out.txt';
+                   return $fh;
+                 });
+
+my $cvtermprop_rs = $track_schema->resultset('Cvtermprop');
+my $cvtermprop_count = $cvtermprop_rs->count();
+
+$processor->process($test_go_obo_file);
+
+my $after_cvtermprop_count = $cvtermprop_rs->count();
+
+is ($cvtermprop_count + 5, $after_cvtermprop_count);
+
+my $prop_rs = $track_schema->resultset('Cvtermprop');
+
+my @new_cvtermprops = ();
+
+while (defined (my $prop = $prop_rs->next())) {
+  if ($prop->type()->name() eq 'canto_subset') {
+    push @new_cvtermprops,
+      [$prop->cvterm()->name(), $prop->value()];
+  };
+}
+
+@new_cvtermprops =
+  sort {
+   $a->[0] cmp $b->[1];
+  } @new_cvtermprops;
+
+cmp_deeply(\@new_cvtermprops,
+           [
+             [
+               'cell phenotype',
+               'FYPO:0000002'
+             ],
+             [
+               'transport vesicle',
+               'GO:0016023'
+             ],
+             [
+               'stored secretory granule',
+               'GO:0016023'
+             ],
+             [
+               'cytoplasmic membrane-bounded vesicle',
+               'GO:0016023'
+             ],
+             [
+               'transmembrane transporter activity',
+               'GO:0022857'
+             ]
+           ]);
