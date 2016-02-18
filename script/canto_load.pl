pombase/canto

commit c24e7d0d741e070e2c086558d5b65b0e733dc92a

Split ExtensionSubsetProcess into two classes
Refs #1075

script/canto_load.pl
@@ -28,7 +28,7 @@ use Canto::Track::OntologyIndex;
 use Canto::Track::LoadUtil;
 use Canto::Track::PubmedUtil;
 use Canto::Curs::TermUpdate;
-use Canto::Config::ExtensionSubsetProcess;
+use Canto::Config::ExtensionProcess;
 
 my $do_genes = 0;
 my $do_pubmed_xml = 0;
@@ -184,13 +184,13 @@ if (@ontology_args) {
   $index->initialise_index();
   my @relationships_to_load = @{$config->{load}->{ontology}->{relationships_to_load}};
 
-  my $extension_subset_process = undef;
+  my $extension_process = undef;
   my $subset_data = undef;
 
   if ($do_process_extension_config) {
-    $extension_subset_process =
-      Canto::Config::ExtensionSubsetProcess->new(config => $config);
-    $subset_data = $extension_subset_process->get_subset_data(@ontology_args);
+    $extension_process =
+      Canto::Config::ExtensionProcess->new(config => $config);
+    $subset_data = $extension_process->get_subset_data(@ontology_args);
   }
 
   my $ontology_load = Canto::Track::OntologyLoad->new(schema => $schema,
@@ -202,11 +202,11 @@ if (@ontology_args) {
   my ($root_terms) = $ontology_load->load([@ontology_args], $index, $synonym_types);
 
   if ($subset_data) {
-    $extension_subset_process->add_to_subset_data($subset_data, 'canto_root_subset',
-                                                  $root_terms);
+    $extension_process->add_to_subset_data($subset_data, 'canto_root_subset',
+                                           $root_terms);
     # add canto_subset cvtermprop to the terms in subsets
-    $extension_subset_process->process_subset_data($ontology_load->load_schema(),
-                                                   $subset_data);
+    my $subset_process = Canto::Chado::SubsetProcess->new();
+    $subset_process->process_subset_data($ontology_load->load_schema(), $subset_data);
   }
 
   if (!$dry_run) {

lib/Canto/Chado/SubsetProcess.pm
@@ -1,0 +1,139 @@
+package Canto::Chado::SubsetProcess;
+
+=head1 NAME
+
+Canto::Chado::SubsetProcess - Store subset data in the DB
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Canto::Chado::SubsetProcess
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use Moose;
+
+=head2 add_to_subset_data
+
+ Usage   : $self->add_to_subset_data($subset_data, 'subset_name', \@subset_ids);
+ Function: Add a new subset to $subset_data
+ Args    : $subset_data - returned by get_subset_data()
+           $subset_name
+           $subset_ids - a array ref
+ Return  : None
+
+=cut
+
+sub add_to_subset_data
+{
+  my $self = shift;
+
+  my $subset_data = shift;
+  my $subset_name = shift;
+  my $subset_ids = shift;
+
+  for my $subset_id (@$subset_ids) {
+    $subset_data->{$subset_id}{$subset_name} = 1;
+  }
+}
+
+
+=head2 process_subset_data
+
+ Usage   : my $subset_data = $extension_process->get_subset_data();
+           my $subset_process = Canto::Chado::SubsetProcess->new();
+           $subset_process->process_subset_data($track_schema, $subset_data);
+ Function: Use the results of get_subset_data() to add a canto_subset
+           cvtermprop for each config file term it's a child of.  For
+           more details see:
+           https://github.com/pombase/canto/wiki/AnnotationExtensionConfig
+ Args    : $track_schema - the database to load
+           $subset_data - A map returned by subset_data()
+ Return  : None - dies on failure
+
+=cut
+
+sub process_subset_data
+{
+  my $self = shift;
+  my $schema = shift;
+  my $subset_data = shift;
+
+  my %db_names = ();
+
+  map {
+    if (/(\w+):/) {
+      $db_names{$1} = 1;
+    }
+  } keys %$subset_data;
+
+  my @db_names = keys %db_names;
+
+  my $cvterm_rs =
+    $schema->resultset('Cvterm')->search({
+      'db.name' => { -in => \@db_names },
+    }, {
+      join => { dbxref => 'db' },
+      prefetch => { dbxref => 'db' }
+    });
+
+  my $canto_subset_term =
+    $schema->resultset('Cvterm')->find({ name => 'canto_subset',
+                                         'cv.name' => 'cvterm_property_type' },
+                                       {
+                                         join => 'cv' });
+
+  while (defined (my $cvterm = $cvterm_rs->next())) {
+    my $db_accession = $cvterm->db_accession();
+
+    my $prop_rs =
+      $cvterm->cvtermprop_cvterms()
+      ->search({
+        type_id => $canto_subset_term->cvterm_id(),
+      });
+
+    $prop_rs->delete();
+
+    my $subset_ids = $subset_data->{$db_accession};
+
+    if ($subset_ids) {
+      my @subset_ids = keys %{$subset_ids};
+
+      for (my $rank = 0; $rank < @subset_ids; $rank++) {
+        my $subset_id = $subset_ids[$rank];
+        $schema->resultset('Cvtermprop')->create({
+          cvterm_id => $cvterm->cvterm_id(),
+          type_id => $canto_subset_term->cvterm_id(),
+          value => $subset_id,
+          rank => $rank,
+        });
+      }
+    }
+  }
+}
+
+1;

lib/Canto/Config/ExtensionProcess.pm
@@ -1,10 +1,9 @@
-package Canto::Config::ExtensionSubsetProcess;
+package Canto::Config::ExtensionProcess;
 
 =head1 NAME
 
-Canto::Config::ExtensionSubsetProcess - Read the domains and ranges from the
-  extension configuration and use owltools to find the child terms.  Store
-  canto_subset cvtermprops to record this.
+Canto::Config::ExtensionProcess - Read the domains and ranges from the
+  extension configuration and use owltools to find the child terms.
 
 =head1 SYNOPSIS
 
@@ -20,7 +19,7 @@ Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc Canto::Config::ExtensionSubsetProcess
+    perldoc Canto::Config::ExtensionProcess
 
 =over 4
 
@@ -75,6 +74,8 @@ sub get_owltools_results
 =head2 get_subset_data
 
  Usage   : my %subset_data = $self->get_subset_data(@obo_file_names);
+           my $subset_process = Canto::Chado::SubsetProcess->new();
+           $subset_process->process_subset_data($track_schema, $subset_data);
  Function: Read the domain and range ontology terms from extension_configuration
            config, then use owtools to find the child terms.
  Args    : @obo_file_names - the OBO files to process with OWLtools
@@ -150,103 +151,5 @@ sub get_subset_data
   return \%subsets;
 }
 
-
-=head2 add_to_subset_data
-
- Usage   : $self->add_to_subset_data($subset_data, 'subset_name', \@subset_ids);
- Function: Add a new subset to $subset_data
- Args    : $subset_data - returned by get_subset_data()
-           $subset_name
-           $subset_ids - a array ref
- Return  : None
-
-=cut
-
-sub add_to_subset_data
-{
-  my $self = shift;
-
-  my $subset_data = shift;
-  my $subset_name = shift;
-  my $subset_ids = shift;
-
-  for my $subset_id (@$subset_ids) {
-    $subset_data->{$subset_id}{$subset_name} = 1;
-  }
-}
-
-
-=head2 process_subset_data
-
- Usage   : my $subset_data = $extension_subset_process->get_subset_data();
-           $extension_subset_process->process_subset_data($track_schema, $subset_data);
- Function: Use the results of get_subset_data() to add a canto_subset
-           cvtermprop for each config file term it's a child of.  For
-           more details see:
-           https://github.com/pombase/canto/wiki/AnnotationExtensionConfig
- Args    : $track_schema - the database to load
-           $subset_data - A map returned by subset_data()
- Return  : None - dies on failure
-
-=cut
-
-sub process_subset_data
-{
-  my $self = shift;
-  my $schema = shift;
-  my $subset_data = shift;
-
-  my %db_names = ();
-
-  map {
-    if (/(\w+):/) {
-      $db_names{$1} = 1;
-    }
-  } keys %$subset_data;
-
-  my @db_names = keys %db_names;
-
-  my $cvterm_rs =
-    $schema->resultset('Cvterm')->search({
-      'db.name' => { -in => \@db_names },
-    }, {
-      join => { dbxref => 'db' },
-      prefetch => { dbxref => 'db' }
-    });
-
-  my $canto_subset_term =
-    $schema->resultset('Cvterm')->find({ name => 'canto_subset',
-                                         'cv.name' => 'cvterm_property_type' },
-                                       {
-                                         join => 'cv' });
-
-  while (defined (my $cvterm = $cvterm_rs->next())) {
-    my $db_accession = $cvterm->db_accession();
-
-    my $prop_rs =
-      $cvterm->cvtermprop_cvterms()
-      ->search({
-        type_id => $canto_subset_term->cvterm_id(),
-      });
-
-    $prop_rs->delete();
-
-    my $subset_ids = $subset_data->{$db_accession};
-
-    if ($subset_ids) {
-      my @subset_ids = keys %{$subset_ids};
-
-      for (my $rank = 0; $rank < @subset_ids; $rank++) {
-        my $subset_id = $subset_ids[$rank];
-        $schema->resultset('Cvtermprop')->create({
-          cvterm_id => $cvterm->cvterm_id(),
-          type_id => $canto_subset_term->cvterm_id(),
-          value => $subset_id,
-          rank => $rank,
-        });
-      }
-    }
-  }
-}
 
 1;

lib/Canto/TestUtil.pm
@@ -42,8 +42,9 @@ use Canto::Track::OntologyIndex;
 use Canto::Track::LoadUtil;
 use Canto::Track::PubmedUtil;
 use Canto::Track::CuratorManager;
-use Canto::Config::ExtensionSubsetProcess;
+use Canto::Config::ExtensionProcess;
 use Canto::DBUtil;
+use Canto::Chado::SubsetProcess;
 
 use Moose;
 
@@ -1225,9 +1226,9 @@ sub get_mock_subset_processor
   {
     my $self = shift;
     my $config = $self->config();
-    my $extension_subset_process = Canto::Config::ExtensionSubsetProcess->new(config => $config);
+    my $extension_process = Canto::Config::ExtensionProcess->new(config => $config);
 
-    $extension_subset_process = Test::MockObject::Extends->new($extension_subset_process);
+    $extension_process = Test::MockObject::Extends->new($extension_process);
     my $get_owltools_results = sub {
       my @results = ();
       open my $fh, '<', $self->root_dir() . '/t/data/owltools_out.txt';
@@ -1238,9 +1239,9 @@ sub get_mock_subset_processor
       close $fh;
       return @results;
     };
-    $extension_subset_process->mock('get_owltools_results', $get_owltools_results);
+    $extension_process->mock('get_owltools_results', $get_owltools_results);
 
-    return $extension_subset_process;
+    return $extension_process;
   }
 
 
@@ -1253,7 +1254,7 @@ sub get_mock_subset_processor
            $include_ro - if true, load RO too
            $include_fypo - load FYPO if true
            $include_closure_subsets - load closure subsets from
-              ExtensionSubsetProcess::get_subset_data()
+              ExtensionProcess::get_subset_data()
  Return  :
 
 =cut
@@ -1282,15 +1283,15 @@ sub load_test_ontologies
 
   my @relationships_to_load = @{$load_config->{ontology}->{relationships_to_load}};
 
-  my $extension_subset_process = undef;
+  my $extension_process = undef;
   my $subset_data = undef;
 
   if ($include_closure_subsets) {
     my @ontology_args = ($test_go_file, $test_fypo_file, $psi_mod_obo_file,
                          $so_obo_file);
-    $extension_subset_process = $self->get_mock_subset_processor();
+    $extension_process = $self->get_mock_subset_processor();
 
-    $subset_data = $extension_subset_process->get_subset_data(@ontology_args);
+    $subset_data = $extension_process->get_subset_data(@ontology_args);
   }
 
   my $ontology_load =
@@ -1316,8 +1317,9 @@ sub load_test_ontologies
   $ontology_load->load(\@sources, $ontology_index, $synonym_types);
 
   if ($include_closure_subsets) {
-    $extension_subset_process->process_subset_data($ontology_load->load_schema(),
-                                                   $subset_data);
+    my $subset_process = Canto::Chado::SubsetProcess->new();
+    $subset_process->process_subset_data($ontology_load->load_schema(),
+                                         $subset_data);
   }
 
   $ontology_load->finalise();

t/75_extension_process.t
@@ -5,7 +5,7 @@ use Test::Deep;
 use Test::MockObject::Extends;
 
 use Canto::TestUtil;
-use Canto::Config::ExtensionSubsetProcess;
+use Canto::Config::ExtensionProcess;
 
 my $test_util = Canto::TestUtil->new();
 
@@ -26,7 +26,7 @@ my $test_go_obo_file =
 my $mock_request = Test::MockObject->new();
 $mock_request->mock('param', sub { return 'track' });
 
-my $processor = $test_util->get_mock_subset_processor();
+my $extension_processor = $test_util->get_mock_subset_processor();
 
 my $prop_rs = $track_schema->resultset('Cvtermprop');
 my $cvtermprop_count = $prop_rs->count();
@@ -36,12 +36,14 @@ my $subset_prop_rs = $prop_rs
 
 is ($subset_prop_rs->count(), 0);
 
-my $subset_data = $processor->get_subset_data($test_go_obo_file);
+my $subset_data = $extension_processor->get_subset_data($test_go_obo_file);
+my $subset_process = Canto::Chado::SubsetProcess->new();
 
-$processor->add_to_subset_data($subset_data, 'canto_root_subset',
+$subset_process->add_to_subset_data($subset_data, 'canto_root_subset',
                                ['GO:0003674', 'GO:0005575', 'GO:0008150']);
 is ($subset_data->{'GO:0005575'}{canto_root_subset}, 1);
-$processor->process_subset_data($track_schema, $subset_data);
+
+$subset_process->process_subset_data($track_schema, $subset_data);
 
 my $after_cvtermprop_count = $prop_rs->count();
 
@@ -118,7 +120,7 @@ cmp_deeply(\@subset_cvtermprops,
 is ($subset_prop_rs->count(), 12);
 
 # run again to make sure it's repeatable
-$processor->process_subset_data($track_schema, $subset_data);
+$subset_process->process_subset_data($track_schema, $subset_data);
 
 is ($prop_rs->count(), $cvtermprop_count + scalar(@subset_cvtermprops));
 is ($subset_prop_rs->count(), 12);

commit ce44fe9502abb727b7cde154aaffe9b444001ecb

Fix ontology load script broken by c24e7d0d741e
Refs #1075

script/canto_load.pl
@@ -202,10 +202,9 @@ if (@ontology_args) {
   my ($root_terms) = $ontology_load->load([@ontology_args], $index, $synonym_types);
 
   if ($subset_data) {
-    $extension_process->add_to_subset_data($subset_data, 'canto_root_subset',
-                                           $root_terms);
     # add canto_subset cvtermprop to the terms in subsets
     my $subset_process = Canto::Chado::SubsetProcess->new();
+    $subset_process->add_to_subset_data($subset_data, 'canto_root_subset', $root_terms);
     $subset_process->process_subset_data($ontology_load->load_schema(), $subset_data);
   }
 