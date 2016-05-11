pombase/canto

commit 7b565dec872625380e4327dfff2fc22c891e0469

Create absolute URL for storing annotations
The relative URL with ../ was going to the wrong URL if the current URL
ended in "/".

Fixes #1128

root/static/js/canto-modules.js
@@ -2090,7 +2090,8 @@ var ontologyWorkflowCtrl =
       toaster.pop('info', 'Storing annotation ...');
       var promise =
         simpleHttpPost(toaster, $http,
-                     '../set_term/' + $scope.annotationType.name,
+                       CantoGlobals.curs_root_uri + '/feature/genotype/annotate/' +
+                       $attrs.featureId + '/set_term/' + $scope.annotationType.name,
                      CursStateService.asAnnotationDetails());
 
       promise.catch(function() {

commit 429cb54af3758db65880e0cd559e43b11d413812

Don't save a genotype annotation for GO
Bug caused by 7b565dec872625

root/static/js/canto-modules.js
@@ -2098,7 +2098,8 @@ var ontologyWorkflowCtrl =
       toaster.pop('info', 'Storing annotation ...');
       var promise =
         simpleHttpPost(toaster, $http,
-                       CantoGlobals.curs_root_uri + '/feature/genotype/annotate/' +
+                       CantoGlobals.curs_root_uri + '/feature/' +
+                       $scope.annotationType.feature_type + '/annotate/' +
                        $attrs.featureId + '/set_term/' + $scope.annotationType.name,
                      CursStateService.asAnnotationDetails());
 