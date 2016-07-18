pombase/canto

commit e4e5e26a9f79683a07b073f8ef6f6dc1a9bb8437

"Add another group" link
Refs #1131

root/static/js/canto-modules.js
@@ -1749,6 +1749,12 @@ var extensionBuilder =
                         $scope.termDetails = { id: null };
                     });
 
+        $scope.addOrGroup = function() {
+          if ($scope.extension[$scope.extension.length - 1].length != 0) {
+            $scope.extension.push([]);
+          }
+        };
+
         $scope.manualEdit = function() {
           var editPromise =
             openExtensionManualEditDialog($modal, $scope.extension, $scope.matchingConfigurations);
@@ -1984,24 +1990,26 @@ var extensionDisplay =
 canto.directive('extensionDisplay', ['CantoGlobals', extensionDisplay]);
 
 
-var extensionOrPartDisplay =
+var extensionOrGroupDisplay =
   function(CantoGlobals) {
     return {
       scope: {
         extension: '=',
-        orPart: '=',
+        orGroup: '=',
         showDelete: '@',
+        editable: '@',
+        isFirst: '@',
       },
       restrict: 'E',
       replace: true,
-      templateUrl: app_static_path + 'ng_templates/extension_or_part_display.html',
+      templateUrl: app_static_path + 'ng_templates/extension_or_group_display.html',
       controller: function($scope) {
         $scope.app_static_path = CantoGlobals.app_static_path;
-        $scope.deleteAndPart = function(andPart) {
+        $scope.deleteAndGroup = function(andGroup) {
           if ($scope.showDelete) {
-            arrayRemoveOne($scope.orPart, andPart);
-            if ($scope.orPart.length == 0) {
-              arrayRemoveOne($scope.extension, $scope.orPart);
+            arrayRemoveOne($scope.orGroup, andGroup);
+            if ($scope.orGroup.length == 0 && $scope.extension.length > 1) {
+              arrayRemoveOne($scope.extension, $scope.orGroup);
             }
           }
         };
@@ -2009,7 +2017,7 @@ var extensionOrPartDisplay =
     };
   };
 
-canto.directive('extensionOrPartDisplay', ['CantoGlobals', extensionOrPartDisplay]);
+canto.directive('extensionOrGroupDisplay', ['CantoGlobals', extensionOrGroupDisplay]);
 
 
 var ontologyWorkflowCtrl =

root/static/ng_templates/annotation_edit.html
@@ -124,7 +124,7 @@
             </button>
           </span>
           <div ng-if="currentUserIsAdmin">
-            <a href="#" ng-click="manualEdit()">Edit as string (admin only) ...</a>
+            <a href="#" ng-click="manualEdit()">Edit as text (admin only) ...</a>
           </div>
         </td>
       </tr>

root/static/ng_templates/extension_builder.html
@@ -1,5 +1,4 @@
 <div class="curs-extension-builder">
-  <div>
     <div style="padding: 0 0 5px 0">
       <span ng-if="isNewExtension">Can you extend this annotation by providing
         additional details based on the experiments in this paper?</span>
@@ -21,15 +20,21 @@
                                   is-valid="andExtIsValid"></extension-builder>
     </div>
 
-    <div ng-if="currentUserIsAdmin">
-      <a href="#" ng-click="manualEdit()">Edit as string (admin only) ...</a>
-    </div>
-  </div>
-
-  <div ng-if="extension.length > 0" style="padding-top: 10px">
+  <div ng-if="extension.length > 1 || extension[0].length > 0" style="padding-top: 10px">
     <div>Extension added so far:</div>
     <div>
       <extension-display extension="extension" show-delete="true"></extension-display>
+    </div>
+  </div>
+
+  <div ng-if="currentUserIsAdmin" style="padding-top: 15px; font-size: 95%;">
+     Admin only:
+    <div style="padding: 5px; font-size: 95%" ng-show="extension[extension.length - 1].length != 0">
+      <a href="#" ng-click="addOrGroup()">Add another group ...</a>
+    </div>
+
+    <div style="padding: 5px; font-size: 95%">
+      <a href="#" ng-click="manualEdit()">Edit as text ...</a>
     </div>
   </div>
 </div>

root/static/ng_templates/extension_display.html
@@ -1,6 +1,8 @@
 <span>
-  <span ng-repeat="orPart in extension">
-    <extension-or-part-display extension="extension" or-part="orPart" show-delete="{{showDelete}}"></extension-or-part-display>
+  <span ng-repeat="orGroup in extension">
+    <extension-or-group-display is-first="{{$first}}" editable="false" extension="extension"
+                                or-group="orGroup" show-delete="{{showDelete}}">
+    </extension-or-group-display>
     <span ng-show="!$last">| </span>
   </span>
 </span>

root/static/ng_templates/extension_or_group_display.html
@@ -1,0 +1,11 @@
+<span>
+  <span ng-repeat="andGroup in orGroup">
+    <span class="curs-extension-relation">{{andGroup.relation}}</span>
+    <span>{{andGroup.rangeDisplayName || andGroup.rangeValue}}</span><span ng-if="andGroup.rangeType == '%'">%</span>
+    <a ng-if="showDelete" confirm="Really delete?" href="" ng-click="deleteAndGroup(andGroup)">
+      <img title="Remove this relation" ng-src="{{app_static_path + '/images/delete_icon.png'}}" />
+    </a>
+    <span ng-show="!$last">, <span ng-if="showDelete">&nbsp;</span></span>
+  </span>
+  <span ng-show="orGroup.length == 0 && isFirst == 'false'">[empty]</span>
+</span>

root/static/ng_templates/extension_or_part_display.html
@@ -1,10 +1,0 @@
-<span>
-  <span ng-repeat="andPart in orPart">
-    <span class="curs-extension-relation">{{andPart.relation}}</span>
-    <span>{{andPart.rangeDisplayName || andPart.rangeValue}}</span><span ng-if="andPart.rangeType == '%'">%</span>
-    <a ng-if="showDelete" confirm="Really delete?" href="" ng-click="deleteAndPart(andPart)">
-      <img title="Remove this relation" ng-src="{{app_static_path + '/images/delete_icon.png'}}" />
-    </a>
-    <span ng-show="!$last">, <span ng-if="showDelete">&nbsp;</span></span>
-  </span>
-</span>

commit 8b99613d45f8925de7edbd1a71c056fd3acddb33

Fix cardinality checking broken by e4e5e26a9f796
Refs #1131

root/static/js/canto-modules.js
@@ -1538,8 +1538,7 @@ var extensionOrGroupBuilder =
                         newCounts[key] = 1;
                       }
                     };
-                  if ($scope.orGroup.length > 0) {
-                  $.map($scope.orGroup[0],
+                  $.map($scope.orGroup,
                         function(part) {
                           var matchingRangeConf = null;
                           $.map(relConf.range,
@@ -1578,7 +1577,6 @@ var extensionOrGroupBuilder =
                             }
                           }
                         });
-                  }
                 });
 
           $q.all(promises).then(function() {
@@ -1639,7 +1637,7 @@ var extensionOrGroupBuilder =
           }
         };
 
-        $scope.$watch('extension',
+        $scope.$watch('orGroup',
                       function() {
                         $scope.checkCardinality($scope.matchingConfigurations);
                       }, true);