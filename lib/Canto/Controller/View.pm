pombase/canto

commit 57be020d7c5839971784fff41b38ca2c459182a0

Fix row order broken by 651a5c3554ac
Refs #585

lib/Canto/Controller/View.pm
@@ -85,7 +85,8 @@ sub get_object_by_id_or_name
   my @column_confs =
     Canto::WebUtil::get_column_confs($c, $rs, $class_info);
 
-  $rs = Canto::WebUtil::process_rs_options($rs, $class_info, [@column_confs]);
+  $rs = Canto::WebUtil::process_rs_options($rs, $c->config(),
+                                           $class_info, [@column_confs]);
 
   return $rs->first();
 }
@@ -244,7 +245,7 @@ sub _parse_order_by
  Usage   : order_list_rs($c, $rs, $class_info, $model_name);
  Function: add an appropriate order_by option to a ResultSet, based on the
            configuration, or using the $order_by arg
- Args    : $c - the Catalyst object
+ Args    : $config - the Config object
            $rs - the ResultSet
            $class_info - the class information from the config file for this
                          ResultSet
@@ -257,8 +258,8 @@ sub _parse_order_by
 =cut
 sub order_list_rs
 {
-  my $c = shift;
   my $rs = shift;
+  my $config = shift;
   my $class_info = shift;
   my $model_name = shift;
   my $order_by = shift;
@@ -289,7 +290,7 @@ sub order_list_rs
   my $table = $class_info->{source};
   my $params = {
     order_by =>
-      $formatted_order_by // _get_order_by_field($c->config(),
+      $formatted_order_by // _get_order_by_field($config,
                                                  $model_name, $table),
   };
 
@@ -337,7 +338,7 @@ sub get_list_rs
 
   my $rs = $schema->resultset($class_name)->search($search);
 
-  return order_list_rs($c, $rs, $class_info, $model, $order_by);
+  return order_list_rs($rs, $c->config(), $class_info, $model, $order_by);
 }
 
 =head2 list

lib/Canto/WebUtil.pm
@@ -299,8 +299,11 @@ sub get_field_value
 sub process_rs_options
 {
   my $rs = shift;
+  my $config = shift;
   my $class_info = shift;
   my $column_confs = shift;
+  my $model_name = shift;
+  my $order_by = shift;
 
   my @column_options = ();
 
@@ -321,9 +324,12 @@ sub process_rs_options
   my $result_source = $rs->result_source();
 
   # sort and limit in the sub-query and then add expensive extra columns
-  return $result_source->resultset()
+  my $return_rs = $result_source->resultset()
     ->search({ $table_id_col_name => { -in => $rs->get_column($table_id_col_name)->as_query(), } },
              { '+columns' => [@column_options] });
+
+  return Canto::Controller::View::order_list_rs($return_rs, $config, $class_info,
+                                                $model_name, $order_by);
 }
 
 =head2

root/view/collection.mhtml
@@ -34,7 +34,7 @@ eval {
     die "no class info for $collection_table_name in configuration of $model_name\n";
   }
 
-  $rs = Canto::Controller::View::order_list_rs($c, $rs, $class_info, $model_name);
+  $rs = Canto::Controller::View::order_list_rs($rs, $c->config(), $class_info, $model_name);
 
   if (! defined $collection_title) {
     $collection_title = ucfirst $collection_name;

root/view/list.mhtml
@@ -103,7 +103,8 @@ if ($is_inline) {
 my @column_confs =
   Canto::WebUtil::get_column_confs($c, $rs, $class_info, $context);
 
-$rs = Canto::WebUtil::process_rs_options($rs, $class_info, [@column_confs]);
+$rs = Canto::WebUtil::process_rs_options($rs, $c->config(), $class_info, [@column_confs],
+                                         $model_name, $order_by);
 
 $st->{column_confs} = [@column_confs];
 

commit a6895b8ba66467a63af2473417f539d3c48df196

Fix object page viewing broken by 57be020d7c58
Refs #585

lib/Canto/Controller/View.pm
@@ -86,7 +86,8 @@ sub get_object_by_id_or_name
     Canto::WebUtil::get_column_confs($c, $rs, $class_info);
 
   $rs = Canto::WebUtil::process_rs_options($rs, $c->config(),
-                                           $class_info, [@column_confs]);
+                                           $class_info, [@column_confs],
+                                           $model_name);
 
   return $rs->first();
 }