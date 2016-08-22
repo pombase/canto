pombase/canto

commit 93a0535ab132c3d1e3b34218448a688239344075

Change stats use to curs submitted date from Chado
Refs #1215

lib/Canto/Chado/Utils.pm
@@ -293,16 +293,25 @@ sub _annotator_annotation_counts
 
   my $dbh = $chado_schema->storage()->dbh();
   my $query = <<"EOF";
-SELECT fc.feature_cvterm_id, emailprop.value, dateprop.value
-  FROM feature_cvterm fc
-  JOIN feature_cvtermprop emailprop
-    ON fc.feature_cvterm_id = emailprop.feature_cvterm_id
-  JOIN feature_cvtermprop dateprop
-    ON fc.feature_cvterm_id = dateprop.feature_cvterm_id
- WHERE emailprop.type_id
-       IN (SELECT cvterm_id FROM cvterm WHERE name = 'curator_email')
-   AND dateprop.type_id
-       IN (SELECT cvterm_id FROM cvterm WHERE name = 'date')
+CREATE TEMP TABLE session_submitted_dates AS
+SELECT pub.pub_id, pp.value AS submitted_date
+FROM pub
+JOIN pubprop pp ON pub.pub_id = pp.pub_id
+JOIN cvterm ppt ON ppt.cvterm_id = pp.type_id
+JOIN cv ON ppt.cv_id = cv.cv_id
+WHERE ppt.name = 'canto_session_submitted_date'
+  AND cv.name = 'pubprop_type';
+
+CREATE INDEX session_submitted_dates_idx ON session_submitted_dates (pub_id);
+
+SELECT fc.feature_cvterm_id, emailprop.value, ssd.submitted_date
+FROM feature_cvterm fc
+JOIN feature_cvtermprop emailprop ON fc.feature_cvterm_id = emailprop.feature_cvterm_id
+JOIN session_submitted_dates ssd ON ssd.pub_id = fc.pub_id
+WHERE emailprop.type_id IN
+    (SELECT cvterm_id
+     FROM cvterm
+     WHERE name = 'curator_email');
 EOF
 
   my $sth = $dbh->prepare($query);

commit 569db6d85842d24dd9b077b4ead0a01db90ef6f1

Fix 60_chado_utils.t broken by 93a0535ab132

lib/Canto/Chado/Utils.pm
@@ -301,9 +301,17 @@ JOIN cvterm ppt ON ppt.cvterm_id = pp.type_id
 JOIN cv ON ppt.cv_id = cv.cv_id
 WHERE ppt.name = 'canto_session_submitted_date'
   AND cv.name = 'pubprop_type';
+EOF
+  my $sth = $dbh->prepare($query);
+  $sth->execute() or die "Couldn't execute: " . $sth->errstr;
 
+  $query = <<"EOF";
 CREATE INDEX session_submitted_dates_idx ON session_submitted_dates (pub_id);
+EOF
+  $sth = $dbh->prepare($query);
+  $sth->execute() or die "Couldn't execute: " . $sth->errstr;
 
+  $query = <<"EOF";
 SELECT fc.feature_cvterm_id, emailprop.value, ssd.submitted_date
 FROM feature_cvterm fc
 JOIN feature_cvtermprop emailprop ON fc.feature_cvterm_id = emailprop.feature_cvterm_id
@@ -314,7 +322,7 @@ WHERE emailprop.type_id IN
      WHERE name = 'curator_email');
 EOF
 
-  my $sth = $dbh->prepare($query);
+  $sth = $dbh->prepare($query);
   $sth->execute() or die "Couldn't execute: " . $sth->errstr;
 
   while (my ($id, $email, $date) = $sth->fetchrow_array()) {

t/60_chado_utils.t
@@ -38,6 +38,6 @@ cmp_deeply(\@stats_table,
            [
              [2013, 0, 1, 0, 0],
              [2014, 0, 0, 0, 0],
-             [2015, 0, 0, 1, 2],
-             [2016, 0, 0, 1, 0]
+             [2015, 0, 0, 2, 2],
+             [2016, 0, 0, 0, 0]
            ]);