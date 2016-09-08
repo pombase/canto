39caf4cf07b9aeeff4e61a9133a0fadef85cfebe
Kim Rutherford <kmr44@cam.ac.uk>
2016-09-08 23:00:57 +1200

    Improve stats page title
    Refs #1234

--- lib/Canto/Controller/Stats.pm
+++ lib/Canto/Controller/Stats.pm
@@ -89,7 +89,7 @@ sub annotation : Local {
 
   $st->{hide_breadcrumbs} = 1;
 
-  $st->{title} = "PomBase annotation statistics";
+  $st->{title} = "PomBase literature curation statistics";
   $st->{template} = 'stats/annotation.mhtml';
 
   if (!$ENV{CANTO_DEBUG}) {
