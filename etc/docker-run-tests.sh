19d18cd327e060dd5b7ea369bd28d31da3044936
Kim Rutherford <kmr44@cam.ac.uk>
2018-08-08 22:42:25 +1200

    Fix test runner to use correct Docker image
    Refs #1593

--- etc/docker-run-tests.sh
+++ etc/docker-run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -
 
 docker run --rm -ti --net="host" --mount type=bind,source=$(pwd)/,target=/canto -w=/canto \
-    pombase/canto-base:v6 cd /canto \&& perl Makefile.PL \&& make test
+    pombase/canto-base:v7 cd /canto \&& perl Makefile.PL \&& make test
