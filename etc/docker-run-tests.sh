be716e5053c6d002e17a2b594ec666562ebfa75b
Kim Rutherford <kmr44@cam.ac.uk>
2018-08-08 23:30:58 +1200

    Add missing Perl dependency to base image
    Refs #1593

--- etc/docker-run-tests.sh
+++ etc/docker-run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -
 
 docker run --rm -ti --net="host" --mount type=bind,source=$(pwd)/,target=/canto -w=/canto \
-    pombase/canto-base:v7 /bin/bash -c "cd /canto && perl Makefile.PL && make test"
+    pombase/canto-base:v8 /bin/bash -c "cd /canto && perl Makefile.PL && make test"
