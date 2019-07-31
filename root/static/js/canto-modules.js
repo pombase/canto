c857ae6758597fc80759fb86af012eba9cec5680
Kim Rutherford <kmr@aska.gen.nz>
2019-07-31 23:37:25 +1200

    Trim whitespace when completing genotype names
    Refs #1900

--- root/static/js/canto-modules.js
+++ root/static/js/canto-modules.js
@@ -1672,7 +1672,17 @@ var featureComplete =
 
         input.autocomplete({
           minLength: 1,
-          source: source,
+          source: function(request, response) {
+            var searchVal = request.term.trim();
+            if (searchVal.length > 0) {
+              response($.grep(source,
+                              function(item) {
+                                return item.label.indexOf(searchVal) != -1;
+                              }));
+            } else {
+              response([]);
+            }
+          },
           select: function (event, ui) {
             $timeout(function () {
               $scope.foundCallback({
