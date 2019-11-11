292b72464766b6ea188a32bceafa7d0ba3faef74
Kim Rutherford <kmr@aska.gen.nz>
2019-11-11 17:42:30 +1300

    Fix missing allele notes after setting background
    Refs pombase/canto#2015
    Refs pombase/canto#2079

--- lib/Canto/Curs/AlleleManager.pm
+++ lib/Canto/Curs/AlleleManager.pm
@@ -179,6 +179,7 @@ sub allele_from_json
   my $allele_type = $json_allele->{type};
   my $gene_id = $json_allele->{gene_id};
   my $comment = $json_allele->{comment};
+  my $notes = $json_allele->{notes};
 
   if ($primary_identifier) {
     # lookup existing allele
@@ -293,6 +294,12 @@ sub allele_from_json
 
   set_allele_synonyms($schema, $allele, \@allele_synonyms);
 
+  if ($notes) {
+    while (my ($key, $value) = each %$notes) {
+      $self->_set_note_with_allele($allele, $key, $value);
+    }
+  }
+
   return $allele;
 }
 
@@ -351,6 +358,35 @@ sub create_simple_allele
   return $allele;
 }
 
+sub _set_note_with_allele
+{
+  my $self = shift;
+  my $allele = shift;
+  my $key = shift;
+  my $value = shift;
+
+  my $existing = $allele->allele_notes()->find({ key => $key });
+
+  if ($existing) {
+    if (defined $value) {
+      $existing->value($value);
+      $existing->update();
+    } else {
+      $existing->delete();
+    }
+  } else {
+    if (defined $value) {
+      $self->curs_schema()
+        ->create_with_type('AlleleNote',
+                           {
+                             allele => $allele->allele_id(),
+                             key => $key,
+                             value => $value,
+                           });
+    }
+  }
+}
+
 =head2 set_note
 
  Usage   : $allele_manager->set_note($allele_primary_identifier, $key, $value);
@@ -377,25 +413,6 @@ sub set_note
     die qq(can't find allele with primary_identifier "$allele_primary_identifier");
   }
 
-  my $existing = $allele->allele_notes()->find({ key => $key });
-
-  if ($existing) {
-    if (defined $value) {
-      $existing->value($value);
-      $existing->update();
-    } else {
-      $existing->delete();
-    }
-  } else {
-    if (defined $value) {
-    $self->curs_schema()
-      ->create_with_type('AlleleNote',
-                         {
-                           allele => $allele->allele_id(),
-                           key => $key,
-                           value => $value,
-                         });
-    }
-  }
+  $self->_set_note_with_allele($allele, $key, $value);
 }
 1;
