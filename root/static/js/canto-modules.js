pombase/canto

commit 34713b53b34a297968398d2eb98052de9d9d0987

Show conditions only if a term is selected
Refs #1891

root/static/js/canto-modules.js
@@ -3032,7 +3032,6 @@ var annotationEvidence =
     var directive = {
       scope: {
         evidenceCode: '=',
-        showConditions: '=?',
         withGeneId: '=',
         validEvidence: '=', // true when evidence and with_gene_id are valid
         annotationTypeName: '@',
@@ -3128,12 +3127,6 @@ var annotationEvidence =
               }
 
               $scope.validEvidence = $scope.isValidCodeAndWith();
-
-              if ("showConditions" in $attrs) {
-                $scope.showConditions =
-                  $scope.isValidEvidenceCode() &&
-                  $scope.annotationType && $scope.annotationType.can_have_conditions;
-              }
             });
 
           $scope.validEvidence = $scope.isValidCodeAndWith();
@@ -5978,7 +5971,6 @@ var annotationEditDialogCtrl =
     $scope.matchingConfigurations = [];
     $scope.status = {
       validEvidence: false,
-      showConditions: false,
       showEvidence: true,
     };
     $scope.chooseFeatureType = null;
@@ -6092,6 +6084,12 @@ var annotationEditDialogCtrl =
       }
       return $scope.status.validEvidence;
     };
+
+    $scope.showConditions = function () {
+      return $scope.status.validEvidence &&
+        $scope.annotationType && $scope.annotationType.can_have_conditions &&
+        $scope.annotation.term_ontid;
+    }
 
     $scope.annotationChanged = function () {
       var changesToStore = {};

root/static/ng_templates/annotation_edit.html
@@ -128,7 +128,7 @@
         </td>
       </tr>
 
-      <tr ng-show="status.showConditions">
+      <tr ng-show="showConditions()">
         <td class="title">Conditions</td>
         <td class="curs-evidence-conditions">
           <div ng-if="filteredFeatures && filteredFeatures.length != 0">

commit e8947e6f8339ccaeca32cb5ac0754539840604ad

Fix missing conditions in ontology workflow
Broken by 34713b53b34a297

Refs #1891

root/curs/modules/ontology.mhtml
@@ -42,7 +42,7 @@ for annotating <% $feature_display_name %> with {{termDetails.id}}
                            valid-evidence="data.validEvidence"
                            annotation-type-name="<% $annotation_type_name %>"></annotation-evidence>
 
-      <div ng-if="showConditions" class="curs-evidence-conditions">
+      <div ng-if="showConditions()" class="curs-evidence-conditions">
         Please add experimental conditions.  Conditions are the aspects of the
         experimental setup that are independent of what cells (strain, genotype,
         constructs, etc.) are used.

root/static/js/canto-modules.js
@@ -2931,8 +2931,6 @@ var ontologyWorkflowCtrl =
     $scope.extensionBuilderReady = false;
     $scope.matchingExtensionConfigs = null;
 
-    $scope.showConditions = false;
-
     $scope.extensionBuilderIsValid = true;
 
     $scope.doNotAnnotateCurrentTerm = false;
@@ -3109,6 +3107,11 @@ var ontologyWorkflowCtrl =
 
       return true;
     };
+
+    $scope.showConditions = function () {
+      return $scope.data.validEvidence &&
+        $scope.annotationType && $scope.annotationType.can_have_conditions;
+    }
 
     $scope.storeAnnotation = function () {
       var storePop = toaster.pop({