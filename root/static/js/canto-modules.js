pombase/canto

commit df187bc0d2500359e86db79ac8bca45fca6a0de8

Add a second org. selector for phys. interactions
Refs pombase/canto#2129

root/static/js/canto-modules.js
@@ -6298,15 +6298,21 @@ var annotationEditDialogCtrl =
     $scope.chooseFeatureType = null;
     $scope.organisms = [];
     $scope.selectedOrganism = args.annotation.organism;
+    $scope.selectedOrganismB = null;
     $scope.hideRelationNames = [];
 
     $scope.multiOrganismMode = CantoGlobals.multi_organism_mode;
 
     $scope.selectedOrganism;
     $scope.initialSelectedOrganismId = null;
+    $scope.initialSelectedOrganismBId = null;
 
     if (args.annotation.feature_a_taxonid) {
       $scope.initialSelectedOrganismId = args.annotation.feature_a_taxonid;
+    }
+
+    if (args.annotation.feature_b_taxonid) {
+      $scope.initialSelectedOrganismBId = args.annotation.feature_b_taxonid;
     }
 
     if (args.annotation.organism) {
@@ -6412,6 +6418,7 @@ var annotationEditDialogCtrl =
       })
       .then(function() {
         setFilteredFeatures();
+        setFilteredFeaturesB();
       });
 
 
@@ -6490,6 +6497,23 @@ var annotationEditDialogCtrl =
       $scope.filteredFeaturesDeferred.resolve($scope.filteredFeatures);
     }
 
+    function filterFeaturesB(features) {
+      if ($scope.selectedOrganismB) {
+        $scope.filteredFeaturesB =
+          $.grep(features,
+                 function(feature) {
+                   return feature.organism.taxonid === $scope.selectedOrganismB.taxonid;
+                 });
+      } else {
+        $scope.filteredFeaturesB = features;
+      }
+
+//  set the feature if there's only one possibilty:
+//      if ($scope.filteredFeaturesB.length == 1) {
+//        $scope.annotation.second_feature_id = $scope.filteredFeaturesB[0].feature_id;
+//      }
+    }
+
     function removeAccessoryAlleles(alleleTypes, alleles) {
       return $.grep(alleles,
                     function(allele) {
@@ -6564,10 +6588,38 @@ var annotationEditDialogCtrl =
       }
     }
 
+    function setFilteredFeaturesB () {
+      if (!$scope.annotationType.second_feature_organism_selector) {
+        return;
+      }
+
+      if ($scope.chooseFeatureType === 'gene') {
+        if (!$scope.selectedOrganismB) {
+          return null;
+        }
+
+        CursGeneList.geneList().then(function (results) {
+          filterFeaturesB(results);
+        }).catch(function (err) {
+          toaster.pop('note', "couldn't read the gene list from the server");
+        });
+      } else {
+        console.error('second_feature_organism_selector config option not implemented for: ' +
+                      $scope.chooseFeatureType);
+      }
+    }
+
     $scope.organismSelected = function (organism) {
       $scope.selectedOrganism = organism;
       $scope.allPromise.then(function () {
         setFilteredFeatures();
+      });
+    }
+
+    $scope.organismBSelected = function (organism) {
+      $scope.selectedOrganismB = organism;
+      $scope.allPromise.then(function () {
+        setFilteredFeaturesB();
       });
     }
 
@@ -6576,12 +6628,17 @@ var annotationEditDialogCtrl =
       modal.result.then(function () {
         $scope.allPromise.then(function () {
           setFilteredFeatures();
+          setFilteredFeaturesB();
         });
       });
     };
 
     $scope.isValidOrganism = function () {
       return !!$scope.selectedOrganism;
+    };
+
+    $scope.isValidOrganismB = function () {
+      return !!$scope.selectedOrganismB;
     };
 
     $scope.isValidFeature = function () {
@@ -6682,6 +6739,11 @@ var annotationEditDialogCtrl =
       $q.all([$scope.annotationTypePromise, $scope.filteredFeaturesPromise])
         .then(function (data) {
           var annotationType = data[0];
+
+          if (annotationType.second_feature_organism_selector) {
+            return;
+          }
+
           if (featureId) {
 
             if (annotationType.term_suggestions_annotation_type) {

root/static/ng_templates/annotation_edit.html
@@ -41,7 +41,6 @@
           <div ng-if="featureEditable && filteredFeatures">
             <feature-chooser feature-type="{{chooseFeatureType}}"
                              features="filteredFeatures"
-                             filter-organism="selectedOrganism"
                              chosen-feature-id="annotation.feature_id"></feature-chooser>
             <span ng-show="filteredFeatures.length != 0 && !annotation.feature_id"
                   class="help-block">Please select a {{chooseFeatureType}}</span>
@@ -128,6 +127,25 @@
         </td>
       </tr>
 
+      <tr ng-if="multiOrganismMode && annotationType.second_feature_organism_selector"
+          ng-class="{ 'has-error': !isValidOrganismB() }">
+        <td class="title">
+          Interacting organism
+        </td>
+        <td ng-if="!selectedOrganismB || featureEditable">
+          <organism-selector
+            organisms="organisms"
+            initial-selection-taxon-id="{{initialSelectedOrganismBId}}"
+            organism-selected="organismBSelected(organism)">
+          </organism-selector>
+          <span ng-show="!isValidOrganism()"
+                class="help-block">Please select an organism</span>
+        </td>
+        <td ng-if="selectedOrganismB && !featureEditable">
+          {{selectedOrganism.scientific_name}}
+        </td>
+      </tr>
+
       <tr ng-if="annotationType.category == 'interaction'"
           ng-class="{ 'has-error': !annotation.second_feature_id }">
         <td class="title">
@@ -140,7 +158,6 @@
           <div ng-if="filteredFeaturesB">
             <feature-chooser feature-type="{{chooseFeatureType}}"
                              features="filteredFeaturesB"
-                             filter-organism="selectedOrganism"
                              chosen-feature-id="annotation.second_feature_id"></feature-chooser>
             <span ng-show="filteredFeaturesB.length == 0 && annotationType && annotationType.interaction_same_locus">
               <span ng-show="annotationType.single_allele_only">

commit 3552e7c298f77a37699796bc2fea0d5de2e8ebee

Fix single_locus_only mode, broken by df187bc0d250
The interacting gene drop-down was hidden initially when editing existing
Fly-Canto rescue annotations.

Refs #2129

root/static/js/canto-modules.js
@@ -6569,11 +6569,7 @@ var annotationEditDialogCtrl =
                 };
               filterFeatures(results, filterFunc);
 
-              if (annotationType.single_locus_only) {
-                $scope.filteredFeaturesB = null;
-              } else {
-                $scope.filteredFeaturesB = $scope.filteredFeatures;
-              }
+              $scope.filteredFeaturesB = $scope.filteredFeatures;
             });
           }).catch(function (err) {
             toaster.pop('note', "couldn't read the genotype list from the server");