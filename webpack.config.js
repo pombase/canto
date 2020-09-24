f94c43f98f2187a9d132a08ac13f31b9d40e22cd
James Seager <james.seager@rothamsted.ac.uk>
2020-09-24 09:37:28 +0100

    Add draft webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './root/static/js/canto-modules.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development'
+};
