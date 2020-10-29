a1c9d3b3c2c3796adcfb7ee7c68664199508bd6e
James Seager <james.seager@rothamsted.ac.uk>
2020-10-29 13:06:38 +0000

    Use canto.js as webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './root/static/js/canto-modules.js',
+  entry: './root/static/js/canto.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'root/dist'),
