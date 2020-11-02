2ba88d676a2b4a3d65ad4761879801e2662ce78e
James Seager <james.seager@rothamsted.ac.uk>
2020-11-02 14:53:52 +0000

    Expose jQuery as global variable with ProvidePlugin
    Otherwise tag-it.js (and probably AngularJS) won't cooperate with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './root/static/js/canto-modules.js',
@@ -12,4 +13,11 @@ module.exports = {
       'tag-it': path.resolve(__dirname, 'node_modules/tag-it/js/tag-it.js')
     }
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ],
 };
