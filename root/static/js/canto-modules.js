4997a43e1a81a227e6856ad63688c3f3bb36457e
Kim Rutherford <kmr@aska.gen.nz>
2021-04-09 13:41:10 +1200

    Split interactions into directional and symmetric

--- root/static/js/canto-modules.js
+++ root/static/js/canto-modules.js
@@ -6603,6 +6603,7 @@ var GenotypeInteractionAnnotationTableCtrl =
     return {
       scope: {
         interactions: '=',
+        showPhenotypes: '<',
         allowDeletion: '<',
       },
       restrict: 'E',
@@ -7017,9 +7018,11 @@ var annotationEditDialogCtrl =
                                           $scope.genotypeInteractionInitialData);
 
       newInteractionsPromise.then(function(result) {
-        console.log(result);
-
-        $scope.annotation.interactionAnnotations.push(result);
+        if (result.phenotypeAnnotations.length == 0) {
+          $scope.annotation.symmetric_interaction_annotations.push(result);
+        } else {
+          $scope.annotation.directional_interaction_annotations.push(result);
+        }
       });
     };
 
@@ -7474,8 +7477,11 @@ var annotationEditDialogCtrl =
               interactionInitialDataPromise.then(function(initialData) {
                 if (typeof(initialData) === 'object') {
                   $scope.allowInteractionAnnotations = true;
-                  if (!$scope.annotation.interactionAnnotations) {
-                    $scope.annotation.interactionAnnotations = [];
+                  if (!$scope.annotation.symmetric_interaction_annotations) {
+                    $scope.annotation.symmetric_interaction_annotations = [];
+                  }
+                  if (!$scope.annotation.directional_interaction_annotations) {
+                    $scope.annotation.directional_interaction_annotations = [];
                   }
 
                   $scope.genotypeInteractionInitialData = initialData;
