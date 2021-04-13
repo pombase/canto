d65155573c139ae2b78408748584e25b93010865
Kim Rutherford <kmr@aska.gen.nz>
2021-04-13 15:28:52 +1200

    Add diploid_locus_count and locus_count to API
    This change adds the counts to the results of the API call
    (/ws/genotype/list/) that retrieves genotype details.  The "include_allele"
    parameter is requred.
    
    Refs pombase/canto#1941

--- lib/Canto/Curs/ServiceUtils.pm
+++ lib/Canto/Curs/ServiceUtils.pm
@@ -455,12 +455,23 @@ sub _genotype_details_hash
 
     my @alleles = ();
 
+    my %allele_count_per_locus = ();
+    my $locus_count = 0;
+
     while (defined (my $row = $allele_genotype_rs->next())) {
       my $allele = $row->allele();
       push @alleles, $allele;
       my $diploid = $row->diploid();
       if ($diploid) {
+        if (exists $allele_count_per_locus{$diploid->name()}) {
+          $allele_count_per_locus{$diploid->name()}++;
+        } else {
+          $allele_count_per_locus{$diploid->name()} = 1;
+          $locus_count++;
+        }
         push @{$diploid_names{$allele->allele_id()}}, $diploid->name();
+      } else {
+        $locus_count++;
       }
     }
 
@@ -488,6 +499,17 @@ sub _genotype_details_hash
     } @allele_hashes;
 
     $ret{alleles} = [@allele_hashes];
+
+    $ret{diploid_locus_count} = 0;
+
+    map {
+      my $diploid_name = $_;
+      if ($allele_count_per_locus{$diploid_name} > 1) {
+        $ret{diploid_locus_count}++;
+      }
+    } keys %allele_count_per_locus;
+
+    $ret{locus_count} = $locus_count;
   }
 
   return \%ret;
