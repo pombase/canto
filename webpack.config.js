10e5f5fe0dc68556c339d122b09e0fda89e98f6a
James Seager <james.seager@rothamsted.ac.uk>
2021-04-20 14:34:13 +0100

    Add loader rules for CSS and font files

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,26 @@ module.exports = {
       'tag-it': path.resolve(__dirname, 'root/static/js/tag-it.js')
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
