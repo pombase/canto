096a0d78547928cf0a9699b2b7a6d193fac103fa
James Seager <james.seager@rothamsted.ac.uk>
2021-05-19 15:02:09 +0100

    Don't import CSS for unused jQuery UI widgets

--- root/static/js/index.js
+++ root/static/js/index.js
@@ -2,8 +2,13 @@ import $ from 'jquery';
 import './canto.js';
 import './canto-modules.js';
 
-import 'jquery-ui/themes/base/base.css';
+import 'jquery-ui/themes/base/core.css';
+import 'jquery-ui/themes/base/menu.css';
+import 'jquery-ui/themes/base/autocomplete.css';
+import 'jquery-ui/themes/base/dialog.css';
+import 'jquery-ui/themes/base/tooltip.css';
 import 'jquery-ui/themes/base/theme.css';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'angularjs-toaster/toaster.css';
 import '../css/style.css';
