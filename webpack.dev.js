8e0f4b4c576ade65faa7643a94b683a3e50618eb
James Seager <james.seager@rothamsted.ac.uk>
2021-05-27 14:03:14 +0100

    Rename webpack.config.js to webpack.common.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
+const common = require('./webpack.common.js');
 
 const config = {
   mode: 'development',
