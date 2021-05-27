239cb824ca90beaac13fd82a4eb44be0889b8671
James Seager <james.seager@rothamsted.ac.uk>
2021-05-27 12:44:56 +0100

    Configure terser-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const config = {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        terserOptions: {
+          mangle: false,
+        },
+      }),
+    ],
+  },
 };
 
 module.exports = merge(common, config);
