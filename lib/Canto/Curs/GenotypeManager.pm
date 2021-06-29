pombase/canto

commit 358052ab3338a6e8379209d7c91ad9104fe8f109

Fix order of alleles in interaction table genotype
It now matches the ontology table and the genotype management page.

Refs pombase/canto#2189

lib/Canto/CursDB/Genotype.pm
@@ -276,6 +276,32 @@ sub all_annotations
   return $self->annotations();
 }
 
+sub alleles_in_config_order
+{
+  my $self = shift;
+  my $config = shift;
+
+  my %allele_type_order = ();
+
+  for (my $idx = 0; $idx < @{$config->{allele_type_list}}; $idx++) {
+    my $allele_config = $config->{allele_type_list}->[$idx];
+
+    $allele_type_order{$allele_config->{name}} = $idx;
+  }
+
+  my $allele_genotype_rs = $self->allele_genotypes()
+    ->search({},
+             {
+               prefetch => [qw[diploid allele]]
+             });
+
+  return sort {
+    ($allele_type_order{$a->allele()->type()} // 0)
+      <=>
+    ($allele_type_order{$b->allele()->type()} // 0);
+  } $allele_genotype_rs->all();
+}
+
 sub allele_string
 {
   my $self = shift;
@@ -284,41 +310,43 @@ sub allele_string
 
   my %diploid_groups = ();
 
-  my $allele_genotype_rs = $self->allele_genotypes()
-    ->search({},
-             {
-               prefetch => [qw[diploid allele]]
-             });
+  my @alleles_in_config_order = $self->alleles_in_config_order($config);
 
-  my $haploid_count = 1;
+  my @haploids = ();
 
-  while (defined (my $row = $allele_genotype_rs->next())) {
+  for my $row (@alleles_in_config_order) {
     my $allele = $row->allele();
     my $diploid = $row->diploid();
     if ($diploid) {
       push @{$diploid_groups{$diploid->name()}}, $allele;
     } else {
-      push @{$diploid_groups{"_haploid-" . $haploid_count++}}, $allele;
+      push @haploids, $row->allele();
     }
   }
 
   my @group_names = ();
 
+  my $_make_group_name = sub {
+    my $allele = shift;
+
+    my $long_id = $allele->long_identifier($config);
+    my $gene = $allele->gene();
+    if ($add_gene_primary_names && $gene) {
+      return $gene->primary_identifier() . '-' . $long_id;
+    } else {
+      return $long_id;
+    }
+  };
+
   for my $group_name (sort keys %diploid_groups) {
     push @group_names, (join ' / ',
-                        sort
                         map {
-                          my $long_id = $_->long_identifier($config);
-                          my $gene = $_->gene();
-                          if ($add_gene_primary_names && $gene) {
-                            $gene->primary_identifier() . '-' . $long_id;
-                          } else {
-                            $long_id;
-                          }
+                          my $allele = $_;
+                          $_make_group_name->($allele);
                         } @{$diploid_groups{$group_name}});
   }
 
-  return join " ", sort @group_names;
+  return join " ", ((sort @group_names), map { $_make_group_name->($_) } @haploids);
 }
 
 sub display_name

lib/Canto/Curs/ServiceUtils.pm
@@ -436,7 +436,7 @@ sub _genotype_details_hash
     name => $genotype->name(),
     background => $genotype->background(),
     comment => $genotype->comment(),
-    allele_string => $genotype->allele_string(),
+    allele_string => $genotype->allele_string($config),
     display_name => $genotype->display_name($config),
     genotype_id => $genotype->genotype_id(),
     annotation_count => $genotype->annotations()->count(),

lib/Canto/Curs/Utils.pm
@@ -629,6 +629,11 @@ sub make_interaction_annotation
   my $genotype_a = $metagenotype->first_genotype();
   my $genotype_b = $metagenotype->second_genotype();
 
+  my %genotype_a_details = _make_genotype_details($schema, $genotype_a, $config,
+                                                  $ontology_lookup, $organism_lookup);
+  my %genotype_b_details = _make_genotype_details($schema, $genotype_b, $config,
+                                                  $ontology_lookup, $organism_lookup);
+
   my $organism_a = $genotype_a->organism();
   my $organism_b = $genotype_b->organism();
 
@@ -703,19 +708,19 @@ sub make_interaction_annotation
       organism => $organism_hash,
       annotation_type => $annotation_type,
       annotation_type_display_name => $annotation_type_display_name,
-      genotype_a_display_name => $genotype_a->display_name($config),
+      genotype_a_display_name => $genotype_a_details{genotype_display_name},
       genotype_a_id => $genotype_a->genotype_id(),
       genotype_a_taxonid => $organism_a->taxonid(),
-      feature_a_display_name => $genotype_a->display_name($config),
+      feature_a_display_name => $genotype_a_details{genotype_display_name},
       feature_a_id => $genotype_a->genotype_id(),
       feature_a_taxonid => $organism_a->taxonid(),
       genotype_a_gene_ids => \@genotype_a_gene_ids,
       publication_uniquename => $pub_uniquename,
       evidence_code => $evidence_code,
-      genotype_b_display_name => $genotype_b->display_name($config),
+      genotype_b_display_name => $genotype_b_details{genotype_display_name},
       genotype_b_id => $genotype_b->genotype_id(),
       genotype_b_taxonid => $organism_b->taxonid(),
-      feature_b_display_name => $genotype_b->display_name($config),
+      feature_b_display_name => $genotype_b_details{genotype_display_name},
       feature_b_id => $genotype_b->genotype_id(),
       feature_b_taxonid => $organism_b->taxonid(),
       genotype_b_gene_ids => \@genotype_b_gene_ids,

commit 93b2c807a8b89a0a017e7a08c7933eeda2e70dc0

Fix code for storing genotypes
We are now much more careful about checking for existing genotypes when
storing.

Refs pombase/canto#2189

Broken by: 358052ab3338 194b99cf7cec 75d50f67c70b

lib/Canto/Curs/GenotypeManager.pm
@@ -95,44 +95,128 @@ sub _string_or_undef
   return $string // '<__UNDEF__>';
 }
 
+sub allele_hashes_in_config_order
+{
+  my $config = shift;
+  my @allele_hashes = @_;
+
+  my %allele_type_order = ();
+
+  for (my $idx = 0; $idx < @{$config->{allele_type_list}}; $idx++) {
+    my $allele_config = $config->{allele_type_list}->[$idx];
+
+    $allele_type_order{$allele_config->{name}} = $idx;
+  }
+
+  my $sorter = sub {
+    my $a = shift;
+    my $b = shift;
+
+    if (!defined $a->{type} && !defined $b->{type}) {
+      return 0;
+    }
+
+    if (!defined $a->{type}) {
+      return -1;
+    }
+
+    if (!defined $b->{type}) {
+      return 1;
+    }
+
+    my $res =
+      ($allele_type_order{$a->{type}} // 0)
+        <=>
+      ($allele_type_order{$b->{type}} // 0);
+
+    if ($res == 0) {
+      my $a_name = $a->{name};
+      my $a_description = $a->{description};
+      my $a_type = $a->{type};
+      my $a_expression = $a->{expression};
+
+      my $b_name = $b->{name};
+      my $b_description = $b->{description};
+      my $b_type = $b->{type};
+      my $b_expression = $b->{expression};
+
+      "$a_name-$a_description-$a_type-$a_expression"
+        cmp
+      "$b_name-$b_description-$b_type-$b_expression"
+    } else {
+      $res;
+    }
+  };
+
+  return sort { $sorter->($a, $b) } @allele_hashes;
+}
+
 sub _allele_string_from_json
 {
   my $config = shift;
   my $curs_key = shift;
   my $allele_manager = shift;
-  my $alleles_data = shift;
+  my $allele_data = shift;
+
+  my @allele_data_copy = map {
+    my $allele_hash = $_;
+
+    my %ret = %$allele_hash;
+
+    my $allele = $allele_manager->allele_from_json($allele_hash, $curs_key);
+
+    $ret{name} = $allele->name() // 'UNKNOWN';
+    $ret{description} = $allele->description() // 'UNKNOWN';
+    $ret{type} = $allele->type() // 'UNKNOWN';
+    $ret{expression} = $allele->expression() // 'UNKNOWN';
+
+    $ret{allele} = $allele;
+
+    \%ret;
+  } @$allele_data;
+
+  @allele_data_copy = allele_hashes_in_config_order($config, @allele_data_copy);
 
   my %diploid_groups= ();
 
-  my $haplod_count = 1;
+  my @haploids = ();
 
-  for my $allele_data (@$alleles_data) {
-    my $allele = $allele_manager->allele_from_json($allele_data, $curs_key);
-
+  for my $allele_data (@allele_data_copy) {
     if ($allele_data->{diploid_name}) {
-      push @{$diploid_groups{$allele_data->{diploid_name}}}, $allele;
+      push @{$diploid_groups{$allele_data->{diploid_name}}}, $allele_data->{allele};
     } else {
-      push @{$diploid_groups{"_haploid-" . $haplod_count++}}, $allele;
+      push @haploids, $allele_data->{allele};
     }
   }
 
   my @group_names = ();
 
-  for my $group_name (sort keys %diploid_groups) {
-    push @group_names, (join ' / ',
-                        sort
-                        map {
-                          my $gene = $_->gene();
-                          my $long_id = $_->long_identifier($config);
-                          if ($gene) {
-                            $gene->primary_identifier() . '-' . $long_id;
-                          } else {
-                            $long_id;
-                          }
-                        } @{$diploid_groups{$group_name}});
+  my $_make_allele_display_name = sub {
+    my $allele = shift;
+    my $gene = $allele->gene();
+    my $long_id = $allele->long_identifier($config);
+    my $type = '<' . $allele->type() . '>';
+    $long_id .= $type;
+    if ($gene) {
+      return $long_id . '-' . $gene->primary_identifier();
+    } else {
+      return $long_id;
+    }
+  };
+
+  my @diploid_group_names = ();
+
+  for my $diploid_name (sort keys %diploid_groups) {
+    push @diploid_group_names, (join ' / ',
+                                map {
+                                  my $allele = $_;
+                                  $_make_allele_display_name->($allele);
+                                } @{$diploid_groups{$diploid_name}});
   }
 
-  return join " ", sort @group_names;
+  my @haploid_names = map { $_make_allele_display_name->($_) } @haploids;
+
+  return join " ", (@diploid_group_names, @haploid_names);
 }
 
 
@@ -210,7 +294,7 @@ sub find_genotype
 
     next if scalar(@alleles) != scalar(@$search_alleles_data);
 
-    if ($search_allele_string eq $genotype->allele_string($self->config(), 1)) {
+    if ($search_allele_string eq $genotype->allele_string($self->config(), 1, 1)) {
       return $genotype;
     }
   }

lib/Canto/CursDB/Genotype.pm
@@ -295,18 +295,62 @@ sub alleles_in_config_order
                prefetch => [qw[diploid allele]]
              });
 
-  return sort {
-    ($allele_type_order{$a->allele()->type()} // 0)
-      <=>
-    ($allele_type_order{$b->allele()->type()} // 0);
-  } $allele_genotype_rs->all();
+  my $_secondary_sort = sub {
+    my $a = shift;
+    my $b = shift;
+
+    my $a_name = $a->name() // 'UNKNOWN';
+    my $a_description = $a->description() // 'UNKNOWN';
+    my $a_type = $a->type() // 'UNKNOWN';
+    my $a_expression = $a->expression() // 'UNKNOWN';
+
+    my $b_name = $b->name() // 'UNKNOWN';
+    my $b_description = $b->description() // 'UNKNOWN';
+    my $b_type = $b->type() // 'UNKNOWN';
+    my $b_expression = $b->expression() // 'UNKNOWN';
+
+    "$a_name-$a_description-$a_type-$a_expression"
+      cmp
+    "$b_name-$b_description-$b_type-$b_expression"
+  };
+
+  my $sorter = sub {
+    my $a = shift->allele();
+    my $b = shift->allele();
+
+    if (!defined $a->type() && !defined $b->type()) {
+      return $_secondary_sort($a, $b);
+    }
+
+    if (!defined $a->type()) {
+      return -1;
+    }
+
+    if (!defined $b->type()) {
+      return 1;
+    }
+
+    my $res =
+      ($allele_type_order{$a->type()} // 0)
+        <=>
+      ($allele_type_order{$b->type()} // 0);
+
+    if ($res == 0) {
+      $_secondary_sort->($a, $b)
+    } else {
+      $res;
+    }
+  };
+
+  return sort { $sorter->($a, $b) } $allele_genotype_rs->all();
 }
 
 sub allele_string
 {
   my $self = shift;
   my $config = shift;
-  my $add_gene_primary_names = shift;
+  my $add_gene_primary_identifer = shift;
+  my $include_type = shift;
 
   my %diploid_groups = ();
 
@@ -330,9 +374,13 @@ sub allele_string
     my $allele = shift;
 
     my $long_id = $allele->long_identifier($config);
+    if ($include_type) {
+      my $type = '<' . $allele->type() . '>';
+      $long_id .= $type;
+    }
     my $gene = $allele->gene();
-    if ($add_gene_primary_names && $gene) {
-      return $gene->primary_identifier() . '-' . $long_id;
+    if ($add_gene_primary_identifer && $gene) {
+      return $long_id . '-' . $gene->primary_identifier();
     } else {
       return $long_id;
     }
@@ -346,7 +394,7 @@ sub allele_string
                         } @{$diploid_groups{$group_name}});
   }
 
-  return join " ", ((sort @group_names), map { $_make_group_name->($_) } @haploids);
+  return join " ", (@group_names, map { $_make_group_name->($_) } @haploids);
 }
 
 sub display_name