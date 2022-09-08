77f65cf89096d7b11a640737ecd022401ac409b4
Kim Rutherford <kmr@aska.gen.nz>
2022-09-08 16:45:00 +1200

    Clear interactions in "Copy and edit" function
    Refs pombase/canto#2621

--- root/static/js/canto-modules.js
+++ root/static/js/canto-modules.js
@@ -10062,6 +10062,8 @@ var annotationTableRow =
 
         $scope.duplicate = function () {
           var newAnnotation = makeNewAnnotation($scope.annotation);
+          newAnnotation.interaction_annotations = [];
+          newAnnotation.interaction_annotations_with_phenotypes = [];
           startEditing($uibModal, annotation.annotation_type,
             newAnnotation, $scope.featureFilterDisplayName,
             true, true);
