4deb57442389f454aeb3613d10a8de07d0d68010
Kim Rutherford <kmr@aska.gen.nz>
2022-09-14 15:35:00 +1200

    Don't lookup extension display value unnecessarily
    If the server has already given us the extension range display value,
    there's no need to look it up again.
    
    Refs pombase/canto#1160
    Refs pombase/canto#2602

--- root/static/js/canto-modules.js
+++ root/static/js/canto-modules.js
@@ -3161,8 +3161,9 @@ var extensionOrGroupDisplay =
 
         $.map($scope.orGroup,
           function (andGroup) {
-            if (!andGroup.rangeType && andGroup.rangeValue.match('^[A-Z_]+:\\d+') ||
-              andGroup.rangeType && andGroup.rangeType == 'Ontology') {
+            if (!andGroup.rangeDisplayName &&
+                (!andGroup.rangeType && andGroup.rangeValue.match('^[A-Z_]+:\\d+') ||
+                 andGroup.rangeType && andGroup.rangeType == 'Ontology')) {
               CantoService.lookup('ontology', [andGroup.rangeValue], {})
                 .then(function (data) {
                   andGroup.rangeDisplayName = data.name;
