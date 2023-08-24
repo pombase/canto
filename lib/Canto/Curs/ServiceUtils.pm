eef5b851c9fbf19a0211c556c2e530fd2d08398d
Kim Rutherford <kmr@aska.gen.nz>
2023-08-24 17:50:51 +1200

    Fix failing tests
    Refs pombase/canto#2758

--- lib/Canto/Curs/ServiceUtils.pm
+++ lib/Canto/Curs/ServiceUtils.pm
@@ -830,13 +830,16 @@ sub _allele_details_hash
     expression => $allele->expression(),
     display_name => $display_name,
     long_display_name => $long_display_name,
-    external_uniquename => $external_uniquename,
     comment => $allele->comment(),
     allele_id => $allele->allele_id(),
     promoter_gene => $allele->promoter_gene(),
     synonyms => \@synonyms_list,
     notes => \%notes,
   );
+
+  if ($external_uniquename) {
+    $result{external_uniquename} = $external_uniquename;
+  }
 
   if ($allele->type() !~ /^aberration/) {
     $result{gene_id} = $allele->gene()->gene_id();
