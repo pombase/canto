5de90c0561eb14540d0b3579040039e9288dcff3
Kim Rutherford <kmr@aska.gen.nz>
2024-03-13 22:48:30 +1300

    Fixed failing test
    Broken by d853ac0fc1e9

--- lib/Canto/Track/CurationLoad.pm
+++ lib/Canto/Track/CurationLoad.pm
@@ -102,8 +102,7 @@ sub _process_row
   my $uniquename = $Canto::Track::PubmedUtil::PUBMED_PREFIX . ":$pubmed_id";
   my $pub = $self->load_util()->get_pub($uniquename, 'admin_load');
   my $lab_head = $self->load_util()->get_person($lab_head_name, $lab_head_email,
-                                                $user_cvterm, $lab_head_email,
-                                                "0000-0000-0001-$suffix");
+                                                "0000-0000-0001-$suffix", $user_cvterm);
   my $lab = $self->load_util()->get_lab($lab_head);
   my $submitter = undef;
 
@@ -111,8 +110,7 @@ sub _process_row
 
   if ($submitter_email) {
     $submitter = $self->load_util()->get_person($submitter_name, $submitter_email,
-                                                $user_cvterm, $submitter_email,
-                                                "0000-0000-0001-$suffix");
+                                                "0000-0000-0001-$suffix", $user_cvterm);
     $suffix++;
   }
 
@@ -151,15 +149,15 @@ sub load
 
   # get_person() creates the person if not found
   $self->load_util()->get_person('Val Wood', 'val@3afaba8a00c4465102939a63e03e2fecba9a4dd7.ac.uk',
-                                 $admin_cvterm, 'val@3afaba8a00c4465102939a63e03e2fecba9a4dd7.ac.uk', '0000-0000-0000-123X');
+                                 '0000-0000-0000-123X', $admin_cvterm);
   my $dr_harris =
     $self->load_util()->get_person('Midori Harris', 'mah79@2b996589fd60a6e63d154d6d33fe9da221aa88e9.ac.uk',
-                                   $admin_cvterm, 'mah79@2b996589fd60a6e63d154d6d33fe9da221aa88e9.ac.uk', '0000-0000-0000-200X');
+                                   '0000-0000-0000-200X', $admin_cvterm);
   $dr_harris->known_as("Dr Harris");
   $dr_harris->update();
 
   $self->load_util()->get_person('Antonia Nilsson', 'a.nilsson@3416497253c29354cb08ec29abe683fc296c35b3.ac.uk',
-                                 $admin_cvterm, 'a.nilsson@3416497253c29354cb08ec29abe683fc296c35b3.ac.uk', '0000-0000-0000-300X');
+                                 '0000-0000-0000-300X', $admin_cvterm);
 
   my $csv = Text::CSV->new({binary => 1});
   open my $curation_io, '<', $curation_file or die;
