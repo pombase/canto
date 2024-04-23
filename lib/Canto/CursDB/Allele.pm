327b73a10aeefcf60468ec270dc7e701bfc14398
Kim Rutherford <kmr@aska.gen.nz>
2024-04-23 16:11:23 +1200

    Fix failing tests

--- lib/Canto/CursDB/Allele.pm
+++ lib/Canto/CursDB/Allele.pm
@@ -72,11 +72,6 @@ __PACKAGE__->table("allele");
   data_type: 'text'
   is_nullable: 1
 
-=head2 exogenous_promoter
-
-  data_type: 'text'
-  is_nullable: 1
-
 =head2 gene
 
   data_type: 'integer'
@@ -103,8 +98,6 @@ __PACKAGE__->add_columns(
   "comment",
   { data_type => "text", is_nullable => 1 },
   "promoter_gene",
-  { data_type => "text", is_nullable => 1 },
-  "exogenous_promoter",
   { data_type => "text", is_nullable => 1 },
   "gene",
   { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
@@ -204,8 +197,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2024-03-18 12:05:26
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:XzWBGRf7AOA0gkFW5u5qcg
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2024-04-23 15:38:48
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:zwunCW+YljT30wPa79v39A
 
 __PACKAGE__->many_to_many('genotypes' => 'allele_genotypes',
                           'genotype');
@@ -253,10 +246,6 @@ sub long_identifier
 
   if ($self->promoter_gene()) {
     $ret .= '{promoter:' . $self->promoter_gene() . '}';
-  }
-
-  if ($self->exogenous_promoter()) {
-    $ret .= '{exogenous_promoter:' . $self->exogenous_promoter() . '}';
   }
 
   return $ret;
